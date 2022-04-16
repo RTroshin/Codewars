e44ddb397d33da98a3e393c5adb99ff18ddec7b2
RTroshin <R.R.Troshin@outlook.com>
2022-04-16 23:55:06 +0300

    Count Odd Numbers below n
    Add new file "Count Odd Numbers below n.c"

---
+++ C/8 kyu/Count Odd Numbers below n.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int input_number(long*);
+long odd_count(long);
+
+int main(void)
+{
+    int n;
+    int rc;
+    
+    printf("Input number: ");
+    rc = input_number(&n);
+    if (rc)
+        return 1;
+
+    printf("Amount Odd numbers below n is %d\n", odd_count(n));
+    
+    return 0;
+}
+
+int input_number(int *n)
+{
+    int rc;
+    
+    rc = scanf("%d", n);
+    if (!rc)
+    {
+        printf("Error! Incorrect enter\n");
+        return 1;
+    }
+    else if (*n < 0)
+    {
+        printf("Error! Incorrect value\n");
+        return 2;
+    }
+    else
+        return 0;
+}
+
+long odd_count(long n) {
+    return n / 2;
+}
