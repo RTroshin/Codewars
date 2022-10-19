0d6dda940bae5929bd7bd33dbac760881344d76a
RTroshin <R.R.Troshin@outlook.com>
2022-10-19 22:30:10 +0300

    Find Nearest square number
    Add new file "Find Nearest square number.py"

---
+++ Python/8 kyu/Find Nearest square number.py
@@ -0,0 +1,6 @@
+def nearest_sq(n):
+    for i in range(0, n):
+        nearest_left = i*i
+        nearest_right = (i + 1)*(i + 1)
+        if nearest_left <= n <= nearest_right:
+            return nearest_left if n - nearest_left < nearest_right - n else nearest_right
