9482fdc759f6f13589033350d03850bc752f0ea2
RTroshin <R.R.Troshin@outlook.com>
2022-10-31 23:45:29 +0300

    Incrementer
    Update file "Incrementer.py"

--- Python/7 kyu/Incrementer.py
+++ Python/7 kyu/Incrementer.py
@@ -1,2 +1,2 @@
 def incrementer(nums):
-    return [num + i + 1 if num + i + 1 < 10 else (num + i + 1) % 10 for num, i in enumerate(nums)]
+    return [(num + i) % 10 for num, i in enumerate(nums, 1)]
