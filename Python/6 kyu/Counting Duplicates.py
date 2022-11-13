62c63b875cc600fc6899c63846dfd40fb24751e2
RTroshin <R.R.Troshin@outlook.com>
2022-11-13 23:05:21 +0300

    Counting Duplicates
    Update file "Counting Duplicates.py"

--- Python/6 kyu/Counting Duplicates.py
+++ Python/6 kyu/Counting Duplicates.py
@@ -1,9 +1,3 @@
 def duplicate_count(text):
-    dupl_let = ""
     text = text.lower()
-
-    for i in text:
-        if text.count(i) > 1 and i not in dupl_let:
-            dupl_let += i
-
-    return len(dupl_let)
+    return len({char: text.count(char) for char in text if text.count(char) > 1})
