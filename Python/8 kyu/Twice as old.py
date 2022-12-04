27e193a7a6e18061cc7a48b0f6d0fdf4164b2402
RTroshin <R.R.Troshin@outlook.com>
2022-12-04 23:55:13 +0300

    Twice as old
    Update file "Twice as old.py"

--- Python/8 kyu/Twice as old.py
+++ Python/8 kyu/Twice as old.py
@@ -1,2 +1,2 @@
 def twice_as_old(dad_years_old, son_years_old):
-    return dad_years_old - 2 * son_years_old if dad_years_old > 2 * son_years_old else 2 * son_years_old - dad_years_old
+    return abs(dad_years_old - 2 * son_years_old)
