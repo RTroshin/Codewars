7c04b3fdbbd2c82dddb7a733211dd2ea63e6c628
RTroshin <R.R.Troshin@outlook.com>
2022-12-09 23:00:05 +0300

    Return a string's even characters
    Update file "Return a string's even characters.py"

--- Python/7 kyu/Return a string's even characters.py
+++ Python/7 kyu/Return a string's even characters.py
@@ -1,2 +1,2 @@
 def even_chars(str):
-    return [str[i] for i in range(1, len(str)) if i % 2] if 1 < len(str) < 101 else "invalid string"
+    return list(str[1::2]) if 1 < len(str) < 101 else "invalid string"
