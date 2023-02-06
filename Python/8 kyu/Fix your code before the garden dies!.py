c2975e81265e05ade2743c11d083aee3356de5c9
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-02-06 23:45:08 +0300

    Fix your code before the garden dies!
    Update file "Fix your code before the garden dies!.py"

--- Python/8 kyu/Fix your code before the garden dies!.py
+++ Python/8 kyu/Fix your code before the garden dies!.py
@@ -1,5 +1,2 @@
 def rain_amount(mm):
-    if mm < 40:
-        return "You need to give your plant " + str(40 - mm) + "mm of water"
-    else:
-        return "Your plant has had more than enough water for today!"
+    return "You need to give your plant " + str(40 - mm) + "mm of water" if mm < 40 else "Your plant has had more than enough water for today!"
