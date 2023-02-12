a6bcec46199de16bebecf21d5c1e2bd256a57c51
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-02-12 23:35:49 +0300

    Polish alphabet
    Add new file "Polish alphabet.py"

---
+++ Python/8 kyu/Polish alphabet.py
@@ -0,0 +1,11 @@
+def correct_polish_letters(str): 
+    polish_alph = {'ą': 'a',
+                   'ć': 'c',
+                   'ę': 'e',
+                   'ł': 'l',
+                   'ń': 'n',
+                   'ó': 'o',
+                   'ś': 's',
+                   'ź': 'z'}
+
+    return ''.join([polish_alph[ch] if ch in "ąćęłńóśź" else ch for ch in str])
