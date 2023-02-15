fcc8f4fd58a4dc2a97235b16279bda375af95102
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-02-15 22:00:09 +0300

    SpeedCode #2 - Array Madness
    Update file "SpeedCode #2 - Array Madness.py"

--- Python/8 kyu/SpeedCode #2 - Array Madness.py
+++ Python/8 kyu/SpeedCode #2 - Array Madness.py
@@ -1,2 +1,2 @@
 def array_madness(a, b):
-    return sum(list(map(lambda num: num**2, a))) > sum(list(map(lambda num: num**3, b)))
+    return sum(list(map(lambda n: n**2, a))) > sum(list(map(lambda n: n**3, b)))
