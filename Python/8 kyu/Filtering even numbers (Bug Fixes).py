a291f068c98dc2ad9ae6893deb0af82bc13cb902
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-03-14 23:50:09 +0300

    Filtering even numbers (Bug Fixes)
    Add new file "Filtering even numbers (Bug Fixes).py"

---
+++ Python/8 kyu/Filtering even numbers (Bug Fixes).py
@@ -0,0 +1,8 @@
+def kata_13_december(lst):
+    # Fix this code
+    j = 0
+    for i in range(len(lst)):
+        if lst[i - j] % 2 == 0:
+            lst.remove(lst[i - j])
+            j += 1
+    return lst
