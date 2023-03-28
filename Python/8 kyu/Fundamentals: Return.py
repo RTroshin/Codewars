6b179e075ef5a994f839825d9788bbee5a91b861
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-03-28 15:55:07 +0300

    Fundamentals: Return
    Add new file "Fundamentals: Return.py"

---
+++ Python/8 kyu/Fundamentals: Return.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+# def mod(a, b):
+#     return
+
+# def exponent(a, b):
+#     return
+
+def subt(a, b):
+    return a - b
