20159d37a3dde768342f04c14bf3b72106fc41e6
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-04-19 22:55:04 +0300

    Grasshopper - Debug
    Update file "Grasshopper - Debug.py"

--- Python/8 kyu/Grasshopper - Debug.py
+++ Python/8 kyu/Grasshopper - Debug.py
@@ -1,9 +1,6 @@
 def weather_info(t):
     c =  convert_to_celsius(t)
-    if c < 0:
-        return str(c) + " is freezing temperature"
-    else:
-        return str(c) + " is above freezing temperature"
+    return str(c) + " is freezing temperature" if c < 0 else str(c) + " is above freezing temperature"
     
 def convert_to_celsius(temperature):
     return (temperature - 32) * (5 / 9)
