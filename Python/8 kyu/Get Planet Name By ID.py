3f22181c7d3e29d961d6890dc917e561e2f6a235
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-05-20 22:25:56 +0300

    Get Planet Name By ID
    Update file "Get Planet Name By ID.py"

--- Python/8 kyu/Get Planet Name By ID.py
+++ Python/8 kyu/Get Planet Name By ID.py
@@ -1,12 +1,10 @@
 def get_planet_name(id):
     match id:
-        case 1: name = "Mercury"
-        case 2: name = "Venus"
-        case 3: name = "Earth"
-        case 4: name = "Mars"
-        case 5: name = "Jupiter"
-        case 6: name = "Saturn"
-        case 7: name = "Uranus"  
-        case 8: name = "Neptune"
-
-    return name
+        case 1: return "Mercury"
+        case 2: return "Venus"
+        case 3: return "Earth"
+        case 4: return "Mars"
+        case 5: return "Jupiter"
+        case 6: return "Saturn"
+        case 7: return "Uranus"  
+        case 8: return "Neptune"
