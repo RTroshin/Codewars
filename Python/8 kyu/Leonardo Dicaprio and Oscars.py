4849f94dd95608029068397d0736484cdde0eac4
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-06-04 23:15:17 +0300

    Leonardo Dicaprio and Oscars
    Add new file "Leonardo Dicaprio and Oscars.py"

---
+++ Python/8 kyu/Leonardo Dicaprio and Oscars.py
@@ -0,0 +1,10 @@
+def leo(oscar):
+    match oscar:
+        case 86:
+            return "Not even for Wolf of wallstreet?!"
+        case 87:
+            return "When will you give Leo an Oscar?"
+        case 88:
+            return "Leo finally won the oscar! Leo is happy"
+        case 89:
+            return "Leo got one already!"
