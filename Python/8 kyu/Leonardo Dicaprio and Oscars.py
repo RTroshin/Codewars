0a132119767f100ab5d326c8407bbbb122cb5d83
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-06-07 21:45:08 +0300

    Leonardo Dicaprio and Oscars
    Update file "Leonardo Dicaprio and Oscars.py"

--- Python/8 kyu/Leonardo Dicaprio and Oscars.py
+++ Python/8 kyu/Leonardo Dicaprio and Oscars.py
@@ -1,7 +1,7 @@
 def leo(oscar):
-    if oscar <= 86:
+    if oscar == 86:
         return "Not even for Wolf of wallstreet?!"
-    elif oscar == 87:
+    elif oscar != 86 and oscar < 88:
         return "When will you give Leo an Oscar?"
     elif oscar == 88:
         return "Leo finally won the oscar! Leo is happy"
