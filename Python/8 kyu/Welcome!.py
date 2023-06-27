8fa69b3f900f2ab216752d064c81707546841682
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-06-27 22:55:12 +0300

    Welcome!
    Update file "Welcome!.py"

--- Python/8 kyu/Welcome!.py
+++ Python/8 kyu/Welcome!.py
@@ -1,20 +1,20 @@
-database = {'english': 'Welcome',
-            'czech': 'Vitejte',
-            'danish': 'Velkomst',
-            'dutch': 'Welkom',
-            'estonian': 'Tere tulemast',
-            'finnish': 'Tervetuloa',
-            'flemish': 'Welgekomen',
-            'french': 'Bienvenue',
-            'german': 'Willkommen',
-            'irish': 'Failte',
-            'italian': 'Benvenuto',
-            'latvian': 'Gaidits',
-            'lithuanian': 'Laukiamas',
-            'polish': 'Witamy',
-            'spanish': 'Bienvenido',
-            'swedish': 'Valkommen',
-            'welsh': 'Croeso'}
+database = {"english": "Welcome",
+            "czech": "Vitejte",
+            "danish": "Velkomst",
+            "dutch": "Welkom",
+            "estonian": "Tere tulemast",
+            "finnish": "Tervetuloa",
+            "flemish": "Welgekomen",
+            "french": "Bienvenue",
+            "german": "Willkommen",
+            "irish": "Failte",
+            "italian": "Benvenuto",
+            "latvian": "Gaidits",
+            "lithuanian": "Laukiamas",
+            "polish": "Witamy",
+            "spanish": "Bienvenido",
+            "swedish": "Valkommen",
+            "welsh": "Croeso"}
 
 def greet(language):
-    return database[language] if language in database else 'Welcome'
+    return database[language] if language in database else "Welcome"
