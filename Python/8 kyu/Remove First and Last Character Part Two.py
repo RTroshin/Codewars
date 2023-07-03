6148bfc65223667def8d54f49ee262f3f7c247de
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-07-03 23:05:03 +0300

    Remove First and Last Character Part Two
    Update file "Remove First and Last Character Part Two.py"

--- Python/8 kyu/Remove First and Last Character Part Two.py
+++ Python/8 kyu/Remove First and Last Character Part Two.py
@@ -1,3 +1,2 @@
 def array(str):
-    str = str.split(',')
-    return ' '.join([str[i] for i in range(1, len(str) - 1)]) if len(str) > 2 else None
+    return ' '.join([ch for ch in str.split(',')[1:-1]]) if len(str) > 4 else None
