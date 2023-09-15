3e4e7209fb8c8ce7af86474b4bae9376ab0d5c83
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-09-15 23:40:46 +0300

    Contamination #1 -String-
    Update file "Contamination #1 -String-.py"

--- Python/8 kyu/Contamination #1 -String-.py
+++ Python/8 kyu/Contamination #1 -String-.py
@@ -1,2 +1,2 @@
 def contamination(text, char):
-    return "".join([char if text and char else "" for i in range(len(text))])
+    return ''.join([char if text and char else '' for i in range(len(text))])
