63108a4b312305a246642ca9b1709637987d0d00
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-09-22 23:15:18 +0300

    Fix the loop!
    Update file "Fix the loop!.py"

--- Python/8 kyu/Fix the loop!.py
+++ Python/8 kyu/Fix the loop!.py
@@ -2,6 +2,6 @@ def list_animals(animals):
     list = ''
 
     for i in range(len(animals)):
-        list += str(i + 1) + '. ' + animals[i] + '\n'
+        list += str(i + 1) + ". " + animals[i] + '\n'
 
     return list
