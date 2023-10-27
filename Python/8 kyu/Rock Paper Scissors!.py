12d9ffe783de01e84927a75bb67884cdc4f6f91a
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-10-27 23:45:21 +0300

    Rock Paper Scissors!
    Update file "Rock Paper Scissors!.py"

--- Python/8 kyu/Rock Paper Scissors!.py
+++ Python/8 kyu/Rock Paper Scissors!.py
@@ -1,11 +1,11 @@
 def rps(p1, p2):
     if p1 == p2:
-        return 'Draw!'
+        return "Draw!"
     elif len(p1) * 2 == len(p2):
-        return 'Player 1 won!'
+        return "Player 1 won!"
     elif len(p2) * 2 == len(p1):
-        return 'Player 2 won!'
+        return "Player 2 won!"
     elif len(p1) > len(p2):
-        return 'Player 1 won!'
+        return "Player 1 won!"
     else:
-        return 'Player 2 won!' 
+        return "Player 2 won!"
