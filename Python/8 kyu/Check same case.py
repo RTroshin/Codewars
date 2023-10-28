0799144b59e5dc34b1f8360c044bddb9177b0fc8
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-10-28 23:45:19 +0300

    Check same case
    Update file "Check same case.py"

--- Python/8 kyu/Check same case.py
+++ Python/8 kyu/Check same case.py
@@ -1,5 +1,5 @@
 def same_case (a, b):
-    if not a.isalpha() or not b.isalpha():
-        return -1
-    else:
+    if a.isalpha() and b.isalpha():
         return a.isupper() == b.isupper()
+    else:
+        return -1
