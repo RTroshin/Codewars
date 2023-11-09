1d561df81467d3379fe0b8179e78a27eefffacf7
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-11-09 23:50:30 +0300

    Logical calculator
    Update file "Logical calculator.py"

--- Python/8 kyu/Logical calculator.py
+++ Python/8 kyu/Logical calculator.py
@@ -1,14 +1,14 @@
 def logical_calc(arr, op):
     match op:
-        case 'AND':
+        case "AND":
             for i in arr[1:]:
                 arr[0] &= i
             return arr[0]
-        case 'OR':
+        case "OR":
             for i in arr[1:]:
                 arr[0] |= i
             return arr[0]
-        case 'XOR':
+        case "XOR":
             for i in arr[1:]:
                 arr[0] ^= i
             return arr[0]
