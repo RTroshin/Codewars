9a85a0936c821a716a14ce5332b182d0fc63f01a
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-11-18 23:30:16 +0300

    Anagram difference
    Update file "Anagram difference.py"

--- Python/6 kyu/Anagram difference.py
+++ Python/6 kyu/Anagram difference.py
@@ -1,9 +1,2 @@
 def anagram_difference(w1, w2):
-    res1, res2 = [], []
-    for ch1 in set(w1):
-        if ch1 in w2:
-            res1 += [ch1]
-    for ch2 in set(w2):
-        if ch2 in w1:
-            res2 += [ch2]
-    return len(w1 + w2) - len(res1 + res2)
+    return len(w1 + w2) - len([ch for ch in set(w1) if ch in w2] + [ch for ch in set(w2) if ch in w1])
