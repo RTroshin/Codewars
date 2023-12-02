f7a8c9645a3d9d468f79bd767b47603074eb69c7
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-12-02 23:00:20 +0300

    Split Strings
    Update file "Split Strings.py"

--- Python/6 kyu/Split Strings.py
+++ Python/6 kyu/Split Strings.py
@@ -1,4 +1,2 @@
 def solution(str):
-    if len(str) % 2:
-        str += '_'
-    return [str[i:i + 2] for i in range(0, len(str), 2)]
+    return [(str + '_')[i:i + 2] for i in range(0, len(str), 2)]
