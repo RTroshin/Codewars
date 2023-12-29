86185dfe00dac23d58bd20ad5d9c70df31dc66b4
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2023-12-29 21:15:12 +0300

    Sum Array
    Update file "Sum Array.py"

--- Python/8 kyu/Sum Array.py
+++ Python/8 kyu/Sum Array.py
@@ -1,8 +1,5 @@
 def sum_array(a):
-    if len(a):
-        sum = 0
-        for i in range(len(a)):
-            sum += a[i]
-        return sum
-    else:
-        return 0
+    sum = 0
+    for i in range(len(a)):
+        sum += a[i]
+    return sum
