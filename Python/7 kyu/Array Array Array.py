6faa0f4897a52add92979ec532965d4d4fdd8070
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-01-01 19:01:00 +0300

    Array Array Array
    Update file "Array Array Array.py"

--- Python/7 kyu/Array Array Array.py
+++ Python/7 kyu/Array Array Array.py
@@ -4,4 +4,4 @@ def explode(arr):
     elif type(arr[0]) != type(arr[1]):
         return [arr for i in range(arr[0])] if type(arr[0]) == int else [arr for i in range(arr[1])]
     else:
-        return 'Void!'
+        return "Void!"
