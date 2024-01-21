8261393951b736c5c5ceeafd12de45d239c7696c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-01-21 23:35:52 +0300

    Root the Num and Find the Integer Sum
    Update file "Root the Num and Find the Integer Sum.py"

--- Python/6 kyu/Root the Num and Find the Integer Sum.py
+++ Python/6 kyu/Root the Num and Find the Integer Sum.py
@@ -1,4 +1,4 @@
 import math
 
 def root_sum(n):
-    return n if n < 2 else sum([root for root in [n ** (1 / i) for i in range(1, 31)] if abs(round(root) - root) < 1e-5])
+    return n if n < 2 else sum(root for root in [n ** (1 / i) for i in range(1, 31)] if abs(round(root) - root) < 1e-5)
