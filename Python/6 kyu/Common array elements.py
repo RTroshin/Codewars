4aa9d96841936b9ba05bedfce8540ebfdef41210
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-01-28 18:55:18 +0300

    Common array elements
    Update file "Common array elements.py"

--- Python/6 kyu/Common array elements.py
+++ Python/6 kyu/Common array elements.py
@@ -3,4 +3,4 @@ def common(a,b,c):
     a = [i for i in a if i in d]
     b = [i for i in b if i in d]
     c = [i for i in c if i in d]
-    return sum([i * min(a.count(i), b.count(i), c.count(i)) for i in d])
+    return sum(i * min(a.count(i), b.count(i), c.count(i)) for i in d)
