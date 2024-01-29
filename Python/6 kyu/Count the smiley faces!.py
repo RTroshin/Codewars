1ab78f856621512e0e49458ab0780c0389d5f9f6
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-01-29 22:35:03 +0300

    Count the smiley faces!
    Update file "Count the smiley faces!.py"

--- Python/6 kyu/Count the smiley faces!.py
+++ Python/6 kyu/Count the smiley faces!.py
@@ -2,9 +2,9 @@ def count_smileys(arr):
     count = 0
 
     for face in arr:
-        if len(face) == 2 and face[0] in ':;' and face[-1] in ')D':
+        if len(face) == 2 and face[0] in ":;" and face[-1] in ")D":
             count += 1
-        elif len(face) == 3 and face[0] in ':;' and face[1] in '-~' and face[-1] in ')D':
+        elif len(face) == 3 and face[0] in ":;" and face[1] in "-~" and face[-1] in ")D":
             count += 1
 
     return count
