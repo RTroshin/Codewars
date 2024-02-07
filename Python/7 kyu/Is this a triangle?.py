a89a46223819a1c3b4d353e8902622a992dc6779
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-02-07 14:35:09 +0300

    Is this a triangle?
    Update file "Is this a triangle?.py"

--- Python/7 kyu/Is this a triangle?.py
+++ Python/7 kyu/Is this a triangle?.py
@@ -1,8 +1,5 @@
 def is_triangle(a, b, c):
     if a > 0 and b > 0 and c > 0:
-        if a >= b + c or b >= a + c or c >= a + b:
-            return False
-        else:
-            return True 
+        return not (a >= b + c or b >= a + c or c >= a + b)
     else:
         return False
