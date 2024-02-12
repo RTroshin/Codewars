7a17226c9497251cd71fdf01890bcf5d8ba94c69
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-02-12 23:15:23 +0300

    String cleaning
    Update file "String cleaning.py"

--- Python/8 kyu/String cleaning.py
+++ Python/8 kyu/String cleaning.py
@@ -2,4 +2,4 @@ def string_clean(str):
     """
     Function will return the cleaned string
     """
-    return ''.join([ch for ch in str if not ch.isdecimal()])
+    return ''.join(ch for ch in str if not ch.isdecimal())
