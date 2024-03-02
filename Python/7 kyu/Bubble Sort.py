8bdcc7715efb34dcd2063f4eb48d56e9463852d0
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-03-02 23:35:23 +0300

    Bubble Sort
    Update file "Bubble Sort.py"

--- Python/7 kyu/Bubble Sort.py
+++ Python/7 kyu/Bubble Sort.py
@@ -4,9 +4,7 @@ def bubble(lst):
     for i in range(len(lst) - 1):
         for j in range(len(lst) - 1):
             if lst[j] > lst[j + 1]:
-                tmp = lst[j + 1]
-                lst[j + 1] = lst[j]
-                lst[j] = tmp
-                res_lst += [lst.copy()]
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                res_lst += [lst.copy()] 
 
     return res_lst
