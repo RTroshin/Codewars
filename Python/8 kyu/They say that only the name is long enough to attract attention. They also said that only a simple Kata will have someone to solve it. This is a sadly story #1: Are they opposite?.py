b5aeed8ada6db0c72cde247a12fa48addd1cb9dc
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-03-07 21:45:07 +0300

    They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?
    Update file "They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?.py"

--- Python/8 kyu/They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?.py
+++ Python/8 kyu/They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?.py
@@ -1,2 +1,2 @@
 def is_opposite(s1, s2):
-    return s1 == ''.join([ch.upper() if ch.islower() else ch.lower() for ch in s2]) if s1 and s2 else False
+    return s1 == ''.join(ch.upper() if ch.islower() else ch.lower() for ch in s2) if s1 and s2 else False
