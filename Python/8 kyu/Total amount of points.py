fad67d4da3316f5b734b534d6d26eac03d87f082
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-03-08 20:15:22 +0300

    Total amount of points
    Update file "Total amount of points.py"

--- Python/8 kyu/Total amount of points.py
+++ Python/8 kyu/Total amount of points.py
@@ -1,2 +1,2 @@
 def points(games):
-    return sum([3 if score[0] > score[-1] else 1 if score[0] == score[-1] else 0 for score in games])
+    return sum(3 if score[0] > score[-1] else 1 if score[0] == score[-1] else 0 for score in games)
