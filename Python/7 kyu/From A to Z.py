4c19125af9abe046527f2b2738f38ad07c437179
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-03-12 14:55:40 +0300

    From A to Z
    Update file "From A to Z.py"

--- Python/7 kyu/From A to Z.py
+++ Python/7 kyu/From A to Z.py
@@ -1,5 +1,4 @@
-alph_low = "abcdefghijklmnopqrstuvwxyz"
-alph_high = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 def gimme_the_letters(sp):
-    return alph_low[alph_low.index(sp.split('-')[0]):alph_low.index(sp.split('-')[1]) + 1] if sp[0].islower() else alph_high[alph_high.index(sp.split('-')[0]):alph_high.index(sp.split('-')[1]) + 1]
+    return alph[alph.index(sp.split('-')[0]):alph.index(sp.split('-')[1]) + 1]
