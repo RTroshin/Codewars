415d58cbdde7043c085d40346a27db850bd1052d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-03-14 20:25:59 +0300

    From A to Z
    Update file "From A to Z.py"

--- Python/7 kyu/From A to Z.py
+++ Python/7 kyu/From A to Z.py
@@ -1,5 +1,4 @@
 alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 def gimme_the_letters(sp):
-    sp = sp.split('-')
-    return alph[alph.index(sp[0]):alph.index(sp[1]) + 1]
+    return alph[alph.index(sp[0]):alph.index(sp[2]) + 1]
