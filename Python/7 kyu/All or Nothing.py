21f4517f1048e22255ee298439629944d22514b0
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-03-28 23:30:08 +0300

    All or Nothing
    Update file "All or Nothing.py"

--- Python/7 kyu/All or Nothing.py
+++ Python/7 kyu/All or Nothing.py
@@ -1,3 +1,3 @@
 def possibly_perfect(key, answers):
-    return True if not [True for i in range(len(key)) if key[i] != '_' and key[i] == answers[i]] or \
-           not [True for i in range(len(key)) if key[i] != '_' and key[i] != answers[i]] else False
+    return not [True for i in range(len(key)) if key[i] != '_' and key[i] == answers[i]] or \
+           not [True for i in range(len(key)) if key[i] != '_' and key[i] != answers[i]]
