a0030c91350b5003696c9769312c57eb5cee6786
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-03-31 23:15:13 +0300

    More than Zero?
    Update file "More than Zero?.py"

--- Python/7 kyu/More than Zero?.py
+++ Python/7 kyu/More than Zero?.py
@@ -1,5 +1,2 @@
 def corrections(x):
-    if x > 0:
-        return str(x) + " is more than zero."
-    else:
-        return str(x) + " is equal to or less than zero."
+    return str(x) + " is more than zero." if x > 0 else str(x) + " is equal to or less than zero."
