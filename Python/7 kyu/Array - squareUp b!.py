2e77c3df6723f1f7d3ece2bbf7cb9a1d4ffe34f4
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-04-14 23:25:07 +0300

    Array - squareUp b!
    Update file "Array - squareUp b!.py"

--- Python/7 kyu/Array - squareUp b!.py
+++ Python/7 kyu/Array - squareUp b!.py
@@ -1,2 +1,2 @@
 def square_up(n):
-    return [j if j <= n - i + 1 else 0 for i in range(n, 0, -1) for j in range(n, 0, -1)]
+    return [0 if j > n - i + 1 else j for i in range(n, 0, -1) for j in range(n, 0, -1)]
