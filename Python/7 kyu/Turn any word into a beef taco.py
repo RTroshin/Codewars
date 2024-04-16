c0a4394f8efbd37b36661dd0d62e8586ee4d5b3d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-04-16 23:35:23 +0300

    Turn any word into a beef taco
    Update file "Turn any word into a beef taco.py"

--- Python/7 kyu/Turn any word into a beef taco.py
+++ Python/7 kyu/Turn any word into a beef taco.py
@@ -1,4 +1,5 @@
 ingredients = {'a': "beef", 'e': "beef",  'i': "beef", 'o': "beef", 'u': "beef", 't': "tomato", 'l': "lettuce", 'c': "cheese", 'g': "guacamole", 's': "salsa"}
 
 def tacofy(word):
-    return ["shell"] + [ingredients.get(ch.lower()) for ch in word if ch.lower() in "aeioutlcgs"] + ["shell"]
+    word = word.lower()
+    return ["shell"] + [ingredients.get(ch) for ch in word if ch in "aeioutlcgs"] + ["shell"]
