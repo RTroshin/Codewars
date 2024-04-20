884ef6b679c99a1cf037189d39ac658bf3bc5651
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-04-20 23:45:49 +0300

    Check the exam
    Add new file "Check the exam.py"

---
+++ Python/7 kyu/Check the exam.py
@@ -0,0 +1,12 @@
+def check_exam(arr1, arr2):
+    res = 0
+
+    for i in range(len(arr1)):
+        if arr1[i] == arr2[i]:
+            res += 4
+        elif arr2[i] == '':
+            res += 0
+        else:
+            res += -1
+
+    return res if res > 0 else 0
