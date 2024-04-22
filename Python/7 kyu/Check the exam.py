8933c3f9340040caa5a6cb8324f4bb8f34596b97
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-04-22 21:35:24 +0300

    Check the exam
    Update file "Check the exam.py"

--- Python/7 kyu/Check the exam.py
+++ Python/7 kyu/Check the exam.py
@@ -1,7 +1,3 @@
 def check_exam(arr1, arr2):
-    res = 0
-
-    for i in range(len(arr1)):
-        res += 4 if arr1[i] == arr2[i] else 0 if arr2[i] == '' else -1
-
+    res = sum(4 if arr1[i] == arr2[i] else 0 if arr2[i] == '' else -1 for i in range(len(arr1)))
     return res if res > 0 else 0
