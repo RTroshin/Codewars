69500e63b1d0b5ccff23c05ea06a4dbaed5efd55
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-04-29 22:35:59 +0300

    Odd-Even String Sort
    Update file "Odd-Even String Sort.py"

--- Python/7 kyu/Odd-Even String Sort.py
+++ Python/7 kyu/Odd-Even String Sort.py
@@ -1,2 +1,2 @@
 def sort_my_string(str):
-    return f"{''.join(str[i] for i in range(len(str)) if not i % 2)} {''.join(str[i] for i in range(len(str)) if i % 2)}"
+    return f"{''.join(str[i] for i in range(0, len(str), 2))} {''.join(str[i] for i in range(1, len(str), 2))}"
