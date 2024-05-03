fe25857cb1ccba35c4283a835f545b0c1920db6d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-03 23:25:22 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe
    Update file "altERnaTIng cAsE <=> ALTerNAtiNG CaSe.py"

--- Python/8 kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe.py
+++ Python/8 kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe.py
@@ -1,2 +1,2 @@
 def to_alternating_case(str):
-    return ''.join([ch.lower() if ch.isupper() else ch.upper() for ch in str])
+    return ''.join(ch.lower() if ch.isupper() else ch.upper() for ch in str)
