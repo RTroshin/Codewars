9b0fec9eeb9ddee69e0bd8113ce06f977d8a5e4c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-06 22:55:30 +0300

    String Reversing, Changing case, etc.
    Update file "String Reversing, Changing case, etc.py"

--- Python/7 kyu/String Reversing, Changing case, etc.py
+++ Python/7 kyu/String Reversing, Changing case, etc.py
@@ -1,4 +1,4 @@
-def reverse_and_mirror(s1, s2):
-    s1 = ''.join(ch.upper() if ch.islower() else ch.lower() for ch in s1)
-    s2 = ''.join(ch.upper() if ch.islower() else ch.lower() for ch in s2)[::-1]
-    return f"{s2}@@@{s1[::-1]}{s1}"
+def reverse_and_mirror(str1, str2):
+    str1 = ''.join(ch.upper() if ch.islower() else ch.lower() for ch in str1)
+    str2 = ''.join(ch.upper() if ch.islower() else ch.lower() for ch in str2)[::-1]
+    return f"{str2}@@@{str1[::-1]}{str1}"
