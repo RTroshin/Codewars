6980d546d4741c2f14771d2a8e3b3038298a7cc1
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-08 23:15:05 +0300

    Pull your words together, man!
    Update file "Pull your words together, man!.py"

--- Python/7 kyu/Pull your words together, man!.py
+++ Python/7 kyu/Pull your words together, man!.py
@@ -1,3 +1,3 @@
 def sentencify(words):
-    words = ' '.join(words) + '.'
-    return f"{words[0].capitalize()}{words[1:]}"
+    words = ' '.join(words)
+    return f"{words[0].capitalize()}{words[1:]}."
