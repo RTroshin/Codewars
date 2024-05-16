f44bd4e045665f5069bbb5107bfba05dba84cd5f
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-16 23:45:09 +0300

    Likes Vs Dislikes
    Update file "Likes Vs Dislikes.py"

--- Python/7 kyu/Likes Vs Dislikes.py
+++ Python/7 kyu/Likes Vs Dislikes.py
@@ -3,8 +3,7 @@ from preloaded import Like, Dislike, Nothing
 def like_or_dislike(lst):
     res_state = Nothing
 
-    if lst:
-        for state in lst:
-            res_state = Nothing if res_state == state else state
+    for state in lst:
+        res_state = Nothing if res_state == state else state
     
     return res_state
