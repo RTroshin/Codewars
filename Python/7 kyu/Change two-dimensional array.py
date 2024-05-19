36568c68e0d2e6128f26d1cff239294537faa13a
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-19 23:26:00 +0300

    Change two-dimensional array
    Update file "Change two-dimensional array.py"

--- Python/7 kyu/Change two-dimensional array.py
+++ Python/7 kyu/Change two-dimensional array.py
@@ -1,5 +1,5 @@
-def matrix(array):
-    for i in range(len(array)):
-        array[i][i] = 1 if array[i][i] > 0 else 0
+def matrix(arr):
+    for i in range(len(arr)):
+        arr[i][i] = 1 if arr[i][i] > 0 else 0
 
-    return array
+    return arr
