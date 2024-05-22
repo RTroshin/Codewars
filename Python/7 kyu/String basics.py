dd64a1aef536c81bb8ab3f6044a8e813a61c0da3
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-22 22:45:15 +0300

    String basics
    Update file "String basics.py"

--- Python/7 kyu/String basics.py
+++ Python/7 kyu/String basics.py
@@ -1,2 +1,2 @@
-def get_users_ids(st):
-    return [ID[3:] for ID in st.strip().replace('#', '').lower().split(", ")]
+def get_users_ids(str):
+    return [ID[3:] for ID in str.strip().replace('#', '').lower().split(", ")]
