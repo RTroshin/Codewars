a846455b9c45fb2ceac1739385dda33fb721cf1c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-27 22:45:18 +0300

    GA-DE-RY-PO-LU-KI cypher
    Update file "GA-DE-RY-PO-LU-KI cypher.py"

--- Python/7 kyu/GA-DE-RY-PO-LU-KI cypher.py
+++ Python/7 kyu/GA-DE-RY-PO-LU-KI cypher.py
@@ -2,10 +2,10 @@ def encode(message):
     return ''.join({'G': 'A', 'D': 'E', 'R': 'Y', 'P': 'O', 'L': 'U', 'K': 'I',
                     'g': 'a', 'd': 'e', 'r': 'y', 'p': 'o', 'l': 'u', 'k': 'i',
                     'A': 'G', 'E': 'D', 'Y': 'R', 'O': 'P', 'U': 'L', 'I': 'K',
-                    'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p', 'u': 'l', 'i': 'k'}.get(ch) if ch.upper() in "GADERYPOLUKI" else ch for ch in message)
+                    'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p', 'u': 'l', 'i': 'k'}.get(ch, ch)for ch in message)
 
 def decode(message):
     return ''.join({'G': 'A', 'D': 'E', 'R': 'Y', 'P': 'O', 'L': 'U', 'K': 'I',
                     'g': 'a', 'd': 'e', 'r': 'y', 'p': 'o', 'l': 'u', 'k': 'i',
                     'A': 'G', 'E': 'D', 'Y': 'R', 'O': 'P', 'U': 'L', 'I': 'K',
-                    'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p', 'u': 'l', 'i': 'k'}.get(ch) if ch.upper() in "GADERYPOLUKI" else ch for ch in message)
+                    'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p', 'u': 'l', 'i': 'k'}.get(ch, ch)for ch in message)
