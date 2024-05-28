ce49e37dec8afd054391d920fd51f8e27e693377
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-05-28 23:35:14 +0300

    GA-DE-RY-PO-LU-KI cypher
    Update file "GA-DE-RY-PO-LU-KI cypher.py"

--- Python/7 kyu/GA-DE-RY-PO-LU-KI cypher.py
+++ Python/7 kyu/GA-DE-RY-PO-LU-KI cypher.py
@@ -1,11 +1,10 @@
+cyphDict = {'G': 'A', 'D': 'E', 'R': 'Y', 'P': 'O', 'L': 'U', 'K': 'I',
+            'g': 'a', 'd': 'e', 'r': 'y', 'p': 'o', 'l': 'u', 'k': 'i',
+            'A': 'G', 'E': 'D', 'Y': 'R', 'O': 'P', 'U': 'L', 'I': 'K',
+            'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p', 'u': 'l', 'i': 'k'}
+
 def encode(message):
-    return ''.join({'G': 'A', 'D': 'E', 'R': 'Y', 'P': 'O', 'L': 'U', 'K': 'I',
-                    'g': 'a', 'd': 'e', 'r': 'y', 'p': 'o', 'l': 'u', 'k': 'i',
-                    'A': 'G', 'E': 'D', 'Y': 'R', 'O': 'P', 'U': 'L', 'I': 'K',
-                    'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p', 'u': 'l', 'i': 'k'}.get(ch, ch)for ch in message)
+    return ''.join(cyphDict.get(ch, ch)for ch in message)
 
 def decode(message):
-    return ''.join({'G': 'A', 'D': 'E', 'R': 'Y', 'P': 'O', 'L': 'U', 'K': 'I',
-                    'g': 'a', 'd': 'e', 'r': 'y', 'p': 'o', 'l': 'u', 'k': 'i',
-                    'A': 'G', 'E': 'D', 'Y': 'R', 'O': 'P', 'U': 'L', 'I': 'K',
-                    'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p', 'u': 'l', 'i': 'k'}.get(ch, ch)for ch in message)
+    return ''.join(cyphDict.get(ch, ch)for ch in message)
