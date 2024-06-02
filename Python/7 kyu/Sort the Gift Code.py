a73f2febec05a7dea21452ae3dc1652fc6e8b0b8
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-06-02 17:20:07 +0300

    Sort the Gift Code
    Update file "Sort the Gift Code.py"

--- Python/7 kyu/Sort the Gift Code.py
+++ Python/7 kyu/Sort the Gift Code.py
@@ -1,9 +1,6 @@
 def sort_gift_code(code):
-    code = list(code)
-
-    for i in range(len(code) - 1):
-        for j in range(len(code) - 1):
-            if ord(code[j]) > ord(code[j + 1]):
-                code[j], code[j + 1] = code[j + 1], code[j]
+    code = [ord(ch) for ch in code]
+    code = sorted(code)
+    code = [chr(ch) for ch in code]
 
     return ''.join(code)
