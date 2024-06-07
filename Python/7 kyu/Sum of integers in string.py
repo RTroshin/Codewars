18fda00ea8c02ae18e3c783955b55f2dd8919e73
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-06-07 21:30:28 +0300

    Sum of integers in string
    Update file "Sum of integers in string.py"

--- Python/7 kyu/Sum of integers in string.py
+++ Python/7 kyu/Sum of integers in string.py
@@ -1,8 +1,10 @@
 def sum_of_integers_in_string(str):
-    new_str = str
+    new_str = ''
 
     for ch in str:
-        if not ch.isdigit():
-            new_str = ' '.join(new_str.split(ch))
+        if ch.isdigit():
+            new_str += ch
+        else:
+            new_str += ' '
 
     return sum(map(int, new_str.split()))
