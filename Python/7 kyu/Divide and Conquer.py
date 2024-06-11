7a013d58b7d9f122c462ba40c32dbd164d8a0cd0
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-06-11 22:45:09 +0300

    Divide and Conquer
    Update file "Divide and Conquer.py"

--- Python/7 kyu/Divide and Conquer.py
+++ Python/7 kyu/Divide and Conquer.py
@@ -1,5 +1,2 @@
 def div_con(x):
-    int_int = [i for i in x if type(i) == int]
-    str_int = [i for i in x if type(i) == str]
-
-    return sum(int_int) - sum(map(int, str_int))
+    return sum([i for i in x if type(i) == int]) - sum(map(int, [i for i in x if type(i) == str]))
