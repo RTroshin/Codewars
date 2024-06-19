20becea06a204173ee53ac2d75d3240a8d8261e5
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-06-19 13:30:27 +0300

    Tunnel Digging
    Update file "Tunnel Digging.py"

--- Python/7 kyu/Tunnel Digging.py
+++ Python/7 kyu/Tunnel Digging.py
@@ -1,17 +1,10 @@
+rock_dict = {'[' : 30, ']' : 30, '{' : 25, '}' : 25, '(' : 20, ')' : 20, '|' : 15, ':' : 10}
+
 def tunnel_digging(r):
     time = 0
 
     for s in r:
-        for c in s:
-            if c in "[]":
-                time += 30
-            elif c in "{}":
-                time += 25
-            elif c in "()":
-                time += 20
-            elif c in "|":
-                time += 15
-            elif c in ":":
-                time += 10
+        for c in s.strip():
+            time += rock_dict.get(c)
     
     return time + (len(r) // 3) * 30
