4cdd37dd31733218329ef22956ca2fce586ab8f4
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-06-24 22:55:05 +0300

    Tunnel Digging
    Update file "Tunnel Digging.py"

--- Python/7 kyu/Tunnel Digging.py
+++ Python/7 kyu/Tunnel Digging.py
@@ -4,4 +4,4 @@ rock_dict = {'[' : 30, ']' : 30,
              '|' : 15, ':' : 10}
 
 def tunnel_digging(r):
-    return sum([rock_dict.get(s) for s in ''.join(r).replace(' ', '')]) + (len(r) // 3) * 30
+    return sum(rock_dict.get(s) for s in ''.join(r).replace(' ', '')) + (len(r) // 3) * 30
