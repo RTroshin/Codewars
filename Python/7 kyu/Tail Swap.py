b02230d4c05895b7abe8dc9e7f66298498661981
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-06-26 21:45:52 +0300

    Tail Swap
    Update file "Tail Swap.py"

--- Python/7 kyu/Tail Swap.py
+++ Python/7 kyu/Tail Swap.py
@@ -1,2 +1,3 @@
 def tail_swap(strings):
-    return [':'.join([strings[0].split(':')[0], strings[1].split(':')[1]])] + [':'.join([strings[1].split(':')[0], strings[0].split(':')[1]])]
+    return [':'.join([strings[0].split(':')[0], strings[1].split(':')[1]])] +\
+           [':'.join([strings[1].split(':')[0], strings[0].split(':')[1]])]
