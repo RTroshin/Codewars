177a2e49bda54d3882db664dc1137197e12d404d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-07-09 21:05:08 +0300

    Student Rankings
    Update file "Student Rankings.py"

--- Python/7 kyu/Student Rankings.py
+++ Python/7 kyu/Student Rankings.py
@@ -5,5 +5,10 @@ def post_grades(students):
         s = s.split('-')
         s1, s2 = s[0].strip(), s[2].split()
         res.append([s1, sum(float(i) for i in s2) / len(s2)])
+    
+    for i in range(len(res) - 1):
+        for j in range(len(res) - 1):
+            if res[j][1] < res[j + 1][1]:
+                res[j][1], res[j + 1][1] = res[j + 1][1], res[j][1]
 
     return res
