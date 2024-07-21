5cfa00034cc86b53065cc027e61548903f6694d0
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-07-21 11:35:17 +0300

    Least Larger
    Update file "Least Larger.py"

--- Python/7 kyu/Least Larger.py
+++ Python/7 kyu/Least Larger.py
@@ -1,8 +1,2 @@
 def least_larger(a, i):
-    res_arr = []
-
-    for j in range(len(a)):
-        if a[i] < a[j]:
-            res_arr.append(a[j])
-
-    return a.index(min(res_arr)) if res_arr else -1
+    return a.index(min(a[j] for j in range(len(a)) if a[i] < a[j])) if a[i] != max(a) else -1
