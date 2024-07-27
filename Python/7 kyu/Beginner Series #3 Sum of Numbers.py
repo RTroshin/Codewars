5a06985c89eb8822d478a3b2fd395755e0f0dea7
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-07-27 21:15:37 +0300

    Beginner Series #3 Sum of Numbers
    Update file "Beginner Series #3 Sum of Numbers.py"

--- Python/7 kyu/Beginner Series #3 Sum of Numbers.py
+++ Python/7 kyu/Beginner Series #3 Sum of Numbers.py
@@ -1,4 +1,2 @@
 def get_sum(a, b):
-    min_n, max_n = min(a, b), max (a, b)
-
-    return sum(range(min_n, max_n + 1)) if a != b else a
+    return sum(range(min(a, b), max (a, b) + 1)) if a != b else a
