00c053c5efda92b5e4f54690beb3242ead60f843
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-08-03 21:30:08 +0300

    Replace all items
    Update file "Replace all items.py"

--- Python/7 kyu/Replace all items.py
+++ Python/7 kyu/Replace all items.py
@@ -1,9 +1,7 @@
 def replace_all(obj, find, repl):
     tp = type(obj)
 
-    if type(obj) != str:
-        obj = ''.join(map(lambda i: str(i), obj))
+    if type(obj) == str:
+        obj = obj.replace(str(find), str(repl))
 
-    obj = obj.replace(str(find), str(repl))
-
-    return obj if tp == str else list(map(lambda i: int(i), obj))
+    return obj if tp == str else len(obj) * [repl]
