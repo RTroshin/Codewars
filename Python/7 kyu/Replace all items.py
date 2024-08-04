aaf24cacee4b13c629a2779e22a8a552a0778798
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-08-04 19:30:19 +0300

    Replace all items
    Update file "Replace all items.py"

--- Python/7 kyu/Replace all items.py
+++ Python/7 kyu/Replace all items.py
@@ -2,6 +2,6 @@ def replace_all(obj, find, repl):
     tp = type(obj)
 
     if type(obj) == str:
-        obj = obj.replace(str(find), str(repl))
+        obj = obj.replace(find, repl)
 
     return obj if tp == str else len(obj) * [repl]
