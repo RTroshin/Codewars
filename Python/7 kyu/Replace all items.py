7c736963107939beac737ff26b297fde6bca5fad
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-08-09 22:25:11 +0300

    Replace all items
    Update file "Replace all items.py"

--- Python/7 kyu/Replace all items.py
+++ Python/7 kyu/Replace all items.py
@@ -1,7 +1,2 @@
 def replace_all(obj, find, repl):
-    if type(obj) != str:
-        for i in range(len(obj)):
-            if obj[i] == find:
-                obj[i] = repl
-
-    return obj.replace(find, repl) if type(obj) == str else obj
+    return obj.replace(find, repl) if type(obj) == str else [n if n != find else repl for n in obj]
