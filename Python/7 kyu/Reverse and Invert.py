393a09aecebcddf2b5b01a06ec28a44ba7c6ba28
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-08-16 23:15:16 +0300

    Reverse and Invert
    Update file "Reverse and Invert.py"

--- Python/7 kyu/Reverse and Invert.py
+++ Python/7 kyu/Reverse and Invert.py
@@ -3,7 +3,6 @@ def reverse_invert(lst):
 
     for i in range(len(lst)):
         if type(lst[i]) == int:
-            sign = -1 if lst[i] > 0 else 1
-            res.append(sign * int((str(abs(lst[i]))[::-1])))
+            res.append((-1 if lst[i] > 0 else 1) * int((str(abs(lst[i]))[::-1])))
 
     return res
