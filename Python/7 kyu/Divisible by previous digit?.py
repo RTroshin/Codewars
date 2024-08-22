31710b4d6792de6b88c213ffc3e1dd01555f817e
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-08-22 21:55:18 +0300

    Divisible by previous digit?
    Update file "Divisible by previous digit?.py"

--- Python/7 kyu/Divisible by previous digit?.py
+++ Python/7 kyu/Divisible by previous digit?.py
@@ -3,6 +3,6 @@ def divisible_by_last(n):
     n = str(n)
 
     for i in range(len(n) - 1):
-        res.append(False) if int(n[i]) == 0 or int(n[i + 1]) % int(n[i]) else res.append(True)
+        res.append(not (int(n[i]) == 0 or int(n[i + 1]) % int(n[i])) or False)
 
     return res
