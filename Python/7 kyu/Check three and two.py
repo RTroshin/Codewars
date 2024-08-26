2733b8e9c02a45c0d3fbbec1df670038ffe88dce
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-08-26 20:20:40 +0300

    Check three and two
    Update file "Check three and two.py"

--- Python/7 kyu/Check three and two.py
+++ Python/7 kyu/Check three and two.py
@@ -1,9 +1,9 @@
-def check_three_and_two(array):
-    if array.count('a') == 3 and (array.count('b') == 2 or array.count('c') == 2):
+def check_three_and_two(arr):
+    if arr.count('a') == 3 and (arr.count('b') == 2 or arr.count('c') == 2):
         return True
-    elif array.count('b') == 3 and (array.count('a') == 2 or array.count('c') == 2):
+    elif arr.count('b') == 3 and (arr.count('a') == 2 or arr.count('c') == 2):
         return True
-    elif array.count('c') == 3 and (array.count('a') == 2 or array.count('b') == 2):
+    elif arr.count('c') == 3 and (arr.count('a') == 2 or arr.count('b') == 2):
         return True
     else:
         return False
