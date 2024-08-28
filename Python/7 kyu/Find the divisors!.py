4fb1fdb62260d627a4fd544f04a27e2c8c97b98b
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-08-28 21:20:32 +0300

    Find the divisors!
    Update file "Find the divisors!.py"

--- Python/7 kyu/Find the divisors!.py
+++ Python/7 kyu/Find the divisors!.py
@@ -1,13 +1,13 @@
 def divisors(integer):
     res = []
 
-    for i in range(2, integer - 1):
+    for i in range(2, integer):
         if not integer % i and i != integer:
             break
     else:
         return f"{integer} is prime"
 
-    for i in range(2, integer - 1):
+    for i in range(2, integer):
         if not integer % i:
             res.append(i)
 
