14c4d62e919f47639dc4ae6ba19106c4f65e9db1
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-09-04 20:25:03 +0300

    L2: Triple X
    Update file "L2: Triple X.py"

--- Python/7 kyu/L2: Triple X.py
+++ Python/7 kyu/L2: Triple X.py
@@ -1,6 +1,6 @@
 def triple_x(str):
     for i, ch in enumerate(str):
-        if ch == 'x' and str[i + 1] == 'x' and str[i + 2] == 'x':
+        if ch == 'x' and str[i + 1] == str[i + 2] == 'x':
             return True
         elif ch == 'x' and str[i + 1] != 'x':
             return False
