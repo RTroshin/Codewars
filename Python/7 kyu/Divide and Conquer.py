b26c7d7bda1ffaff86a0d1aa0a614304c0e2840d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-09-07 22:15:04 +0300

    Divide and Conquer
    Update file "Divide and Conquer.py"

--- Python/7 kyu/Divide and Conquer.py
+++ Python/7 kyu/Divide and Conquer.py
@@ -1,2 +1,2 @@
 def div_con(arr):
-    return sum(n for n in arr if type(n) == int) - sum(map(int, [n for n in arr if type(n) == str]))
+    return sum(n for n in arr if type(n) == int) - sum(int(n) for n in arr if type(n) == str)
