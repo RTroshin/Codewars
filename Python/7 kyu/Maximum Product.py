ae403e5c03020e5411e34244cee0374da8249396
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-09-09 18:35:08 +0300

    Maximum Product
    Update file "Maximum Product.py"

--- Python/7 kyu/Maximum Product.py
+++ Python/7 kyu/Maximum Product.py
@@ -1,8 +1,2 @@
 def adjacent_element_product(arr):
-    max = arr[0] * arr[1]
-
-    for i in range(len(arr) - 1):
-        if max < arr[i] * arr[i + 1]:
-            max = arr[i] * arr[i + 1]
-
-    return max
+    return max(arr[i] * arr[i + 1] for i in range(len(arr) - 1))
