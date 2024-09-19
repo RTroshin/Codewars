e3abfaefd24629a26e50f7dbc05a2170d819f68b
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-09-19 20:20:11 +0300

    Remove duplicate words
    Update file "Remove duplicate words.py"

--- Python/7 kyu/Remove duplicate words.py
+++ Python/7 kyu/Remove duplicate words.py
@@ -1,8 +1,8 @@
 def remove_duplicate_words(str):
-    res = ''
+    res = str.split()[0]
 
     for wrd in str.split():
         if wrd not in res:
             res += ' ' + wrd
 
-    return res.strip()
+    return res
