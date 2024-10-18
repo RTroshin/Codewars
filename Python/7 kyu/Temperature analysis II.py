3bf76b58de94d760aa920b2375c65b77ae7f6a51
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-10-18 23:45:56 +0300

    Temperature analysis II
    Update file "Temperature analysis II.py"

--- Python/7 kyu/Temperature analysis II.py
+++ Python/7 kyu/Temperature analysis II.py
@@ -1,5 +1,2 @@
 def close_to_zero(t):
-    if not t:
-        return 0
-
-    return [n for n in sorted(map(int, t.split())) if n >= 0][0]
+    return [n for n in sorted(map(int, t.split())) if n >= 0][0] if t else 0
