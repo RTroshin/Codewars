a9b261869783971f3a623456a61bf80a038938bb
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-10-22 19:15:38 +0300

    Nothing special
    Update file "Nothing special.py"

--- Python/7 kyu/Nothing special.py
+++ Python/7 kyu/Nothing special.py
@@ -2,10 +2,4 @@ def nothing_special(st):
     if not isinstance(st, str):
         return "Not a string!"
 
-    res = ''
-
-    for ch in st:
-        if ch.isalpha() or ch.isdigit() or ch.isspace():
-            res += ch
-
-    return res
+    return ''.join(ch for ch in st if ch.isalpha() or ch.isdigit() or ch.isspace())
