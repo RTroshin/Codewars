d5ab9b996680666f0d85cef54361dee570f584a9
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-10-25 23:35:11 +0300

    Find the missing element between two arrays
    Update file "Find the missing element between two arrays.py"

--- Python/7 kyu/Find the missing element between two arrays.py
+++ Python/7 kyu/Find the missing element between two arrays.py
@@ -3,7 +3,5 @@ def find_missing(arr1, arr2):
     arr2.sort()
 
     for i, n in enumerate(arr1):
-        if i == len(arr1) - 1:
-            return n
-        elif n != arr2[i]:
+        if i == len(arr1) - 1 or n != arr2[i]:
             return n
