8089987ad5a37ceaddcc465b5b7f3685fbe843b7
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-11-04 21:00:11 +0300

    Alternate capitalization
    Update file "Alternate capitalization.py"

--- Python/7 kyu/Alternate capitalization.py
+++ Python/7 kyu/Alternate capitalization.py
@@ -3,8 +3,8 @@ def capitalize(str):
 
     for i in range(len(str)):
         if i % 2:
-            res_str_2 += str[i].upper()
             res_str_1 += str[i]
+            res_str_2 += str[i].upper()
         else:
             res_str_1 += str[i].upper()
             res_str_2 += str[i]
