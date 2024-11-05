0b4a591b3d6e5165e19e3cc16a4a98d542630657
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-11-05 21:35:14 +0300

    Credit Card Mask
    Add new file "Credit Card Mask.py"

---
+++ Python/7 kyu/Credit Card Mask.py
@@ -0,0 +1,13 @@
+def maskify(cc):
+    if len(cc) < 5:
+        return cc
+
+    res = ''
+
+    for i in range(len(cc)):
+        if i < (len(cc) - 4):
+            res += '#'
+
+    res += cc[-4:]
+
+    return res
