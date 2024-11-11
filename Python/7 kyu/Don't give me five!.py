3224cc288171b1a96a476c42f642a98a579eb550
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-11-11 22:50:33 +0300

    Don't give me five!
    Update file "Don't give me five!.py"

--- Python/7 kyu/Don't give me five!.py
+++ Python/7 kyu/Don't give me five!.py
@@ -1,8 +1,2 @@
-def dont_give_me_five(start,end):
-    res = []
-
-    for i in range(start, end + 1):
-        if '5' not in str(i):
-            res.append(i)
-
-    return len(res)
+def dont_give_me_five(start, end):
+    return len([i for i in range(start, end + 1) if '5' not in str(i)])
