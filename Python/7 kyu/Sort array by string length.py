ee75a72c9b755aa394852d32c3c3b8223131083e
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-11-16 22:20:16 +0300

    Sort array by string length
    Update file "Sort array by string length.py"

--- Python/7 kyu/Sort array by string length.py
+++ Python/7 kyu/Sort array by string length.py
@@ -2,8 +2,6 @@ def sort_by_length(arr):
     for i in range(len(arr) - 1):
         for j in range(len(arr) - 1):
             if len(arr[j]) > len(arr[j + 1]):
-                tmp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = tmp
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
