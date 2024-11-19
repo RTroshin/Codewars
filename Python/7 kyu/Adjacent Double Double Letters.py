c955e95f86612b4c9fc59ef30708f0ad5ec53d19
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-11-19 21:30:09 +0300

    Adjacent Double Double Letters
    Update file "Adjacent Double Double Letters.py"

--- Python/7 kyu/Adjacent Double Double Letters.py
+++ Python/7 kyu/Adjacent Double Double Letters.py
@@ -1,8 +1,8 @@
 def adjacent_double_double_letters(word):
     lenWord = len(word)
 
-    for i in range(lenWord):
-        if i < lenWord - 3 and word[i] == word[i + 1]:
+    for i in range(lenWord - 3):
+        if i < lenWord and word[i] == word[i + 1]:
             if word[i + 2] == word[i + 3]:
                 return True
 
