98bfd6659f2cc456da9b7671d8474742dbff703c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-12-01 19:55:08 +0300

    Maximum Length Difference
    Add new file "Maximum Length Difference.py"

---
+++ Python/7 kyu/Maximum Length Difference.py
@@ -0,0 +1,10 @@
+def mxdiflg(a1, a2):
+    res = []
+
+    for x in a1:
+        for y in a2:
+            res.append(abs(len(x) - len(y)))
+
+    resMax = max(res) if res else -1
+
+    return resMax
