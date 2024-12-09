b989d84363875057d6da9659dbb2de35e2a1121e
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2024-12-09 22:30:05 +0300

    Predict your age!
    Update file "Predict your age!.py"

--- Python/7 kyu/Predict your age!.py
+++ Python/7 kyu/Predict your age!.py
@@ -1,13 +1,7 @@
-def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+def predict_age(*ages):
     ages_lst = []
 
-    ages_lst.append(age_1 * age_1)
-    ages_lst.append(age_2 * age_2)
-    ages_lst.append(age_3 * age_3)
-    ages_lst.append(age_4 * age_4)
-    ages_lst.append(age_5 * age_5)
-    ages_lst.append(age_6 * age_6)
-    ages_lst.append(age_7 * age_7)
-    ages_lst.append(age_8 * age_8)
+    for age in ages:
+        ages_lst.append(age * age)
 
     return (sum(ages_lst) ** 0.5) // 2
