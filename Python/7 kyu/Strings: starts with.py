ed3e5db3e249529d367fb50a0365b2f5152db686
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-01-04 23:25:05 +0300

    Strings: starts with
    Update file "Strings: starts with.py"

--- Python/7 kyu/Strings: starts with.py
+++ Python/7 kyu/Strings: starts with.py
@@ -1,7 +1,3 @@
 def starts_with(st, prefix):
-    if not st and prefix:
-        return False
-
     st = st.split()
-
-return not prefix or st[0] == prefix
+    return False if not st and prefix else not prefix or st[0] == prefix
