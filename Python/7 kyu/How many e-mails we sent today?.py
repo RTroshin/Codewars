bf722a1826532a08e748360c7128908df59a9e25
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-01-19 22:36:01 +0300

    How many e-mails we sent today?
    Update file "How many e-mails we sent today?.py"

--- Python/7 kyu/How many e-mails we sent today?.py
+++ Python/7 kyu/How many e-mails we sent today?.py
@@ -1,9 +1,7 @@
-def get_percentage(sent, limit):
+def get_percentage(sent, limit = 1000):
     if sent == 0:
         return "No e-mails sent"
     elif sent >= limit:
         return "Daily limit is reached"
-    elif not limit:
-        limit = 1000
 
     return str(int(sent / limit * 100)) + '%'
