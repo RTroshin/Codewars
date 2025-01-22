76738e6615e7b77ab9143d39eb660217397e329b
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-01-22 22:25:22 +0300

    Find missing numbers
    Update file "Find missing numbers.py"

--- Python/7 kyu/Find missing numbers.py
+++ Python/7 kyu/Find missing numbers.py
@@ -2,10 +2,4 @@ def find_missing_numbers(arr):
     if len(arr) < 2:
         return []
 
-    res = []
-
-    for i in range(arr[0], arr[-1]):
-        if i not in arr:
-            res.append(i)
-
-    return res
+    return [i for i in range(arr[0], arr[-1]) if i not in arr]
