eb3a67e35e3a09dfe11005425f8f1bc73e132ecd
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-02-10 22:25:22 +0300

    Make the small words big!
    Add new file "Make the small words big!.py"

---
+++ Python/7 kyu/Make the small words big!.py
@@ -0,0 +1,11 @@
+def small_word_helper(sentence):
+    sentence = sentence.split()
+    res = []
+
+    for word in sentence:
+        if len(word) < 4:
+            res.append(''.join([ch.upper() for ch in word]))
+        else:
+            res.append(''.join([ch for ch in word if ch not in "aeiou"]))
+
+    return ' '.join(res)
