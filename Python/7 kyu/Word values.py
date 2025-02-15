6759a0d8d7b857f9f908fb7959bed227bcc1b2a8
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-02-15 23:45:08 +0300

    Word values
    Update file "Word values.py"

--- Python/7 kyu/Word values.py
+++ Python/7 kyu/Word values.py
@@ -1,4 +1,4 @@
-alphabet = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
+alph = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
             'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
             'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
             'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
@@ -8,6 +8,6 @@ def name_value(myList):
     res = []
 
     for i in range(len(myList)):
-        res.append(sum(alphabet[ch] for ch in ''.join(myList[i].split())) * (i + 1))
+        res.append(sum(alph[ch] for ch in ''.join(myList[i].split())) * (i + 1))
 
     return res
