0868e2781a49c75050477ea1bcd3d248d9e76bb9
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-02-21 23:45:09 +0300

    Turkish Numbers, 0-99
    Update file "Turkish Numbers, 0-99.py"

--- Python/7 kyu/Turkish Numbers, 0-99.py
+++ Python/7 kyu/Turkish Numbers, 0-99.py
@@ -1,4 +1,7 @@
-turkish_numbers = {0: "sıfır", 1: "bir", 2: "iki", 3: "üç", 4: "dört", 5: "beş", 6: "altı", 7: "yedi", 8: "sekiz", 9: "dokuz", 10: "on", 20: "yirmi", 30: "otuz", 40: "kırk", 50: "elli", 60: "altmış", 70: "yetmiş", 80: "seksen", 90: "doksan"}
+turkish_numbers = {0: "sıfır", 1: "bir", 2: "iki", 3: "üç", 4: "dört", 5: "beş",
+                   6: "altı", 7: "yedi", 8: "sekiz", 9: "dokuz", 10: "on",
+                   20: "yirmi", 30: "otuz", 40: "kırk", 50: "elli",
+                   60: "altmış", 70: "yetmiş", 80: "seksen", 90: "doksan"}
 
 def get_turkish_number(num):
     if num < 10:
