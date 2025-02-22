6448f77df0bc2be8159cdb7ec8bfb682269a4fc8
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-02-22 22:00:03 +0300

    Turkish Numbers, 0-99
    Update file "Turkish Numbers, 0-99.py"

--- Python/7 kyu/Turkish Numbers, 0-99.py
+++ Python/7 kyu/Turkish Numbers, 0-99.py
@@ -6,21 +6,21 @@ turkish_numbers = {0: "sıfır", 1: "bir", 2: "iki", 3: "üç", 4: "dört", 5: "
 def get_turkish_number(num):
     if num < 10:
         return turkish_numbers.get(num)
-    if num >= 10 and num < 20:
+    elif num >= 10 and num < 20:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 10)
-    if num >= 20 and num < 30:
+    elif num >= 20 and num < 30:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 20)
-    if num >= 30 and num < 40:
+    elif num >= 30 and num < 40:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 30)
-    if num >= 40 and num < 50:
+    elif num >= 40 and num < 50:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 40)
-    if num >= 50 and num < 60:
+    elif num >= 50 and num < 60:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 50)
-    if num >= 60 and num < 70:
+    elif num >= 60 and num < 70:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 60)
-    if num >= 70 and num < 80:
+    elif num >= 70 and num < 80:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 70)
-    if num >= 80 and num < 90:
+    elif num >= 80 and num < 90:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 80)
-    if num >= 90 and num < 100:
+    elif num >= 90 and num < 100:
         return turkish_numbers.get(num // 10 * 10) + ' ' + turkish_numbers.get(num - 90)
