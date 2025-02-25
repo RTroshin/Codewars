f48d9f00e99f7c6196a2f212dcda28728e0d08c7
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-02-25 23:50:20 +0300

    SillyCASE
    Update file "SillyCASE.py"

--- Python/7 kyu/SillyCASE.py
+++ Python/7 kyu/SillyCASE.py
@@ -1,3 +1,3 @@
 def sillycase(silly):
     lenSilly = len(silly)
-    return ''.join([silly[i].lower() if i < lenSilly // 2 else silly[i].upper() for i in range(lenSilly)])
+    return ''.join(silly[i].lower() if i < lenSilly / 2 else silly[i].upper() for i in range(lenSilly))
