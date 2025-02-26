79097b6ad835569241b96fd5fa5111f9bd24565b
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-02-26 23:45:25 +0300

    The old switcheroo
    Update file "The old switcheroo.py"

--- Python/7 kyu/The old switcheroo.py
+++ Python/7 kyu/The old switcheroo.py
@@ -2,9 +2,6 @@ def vowel_2_index(s):
     res = []
 
     for i in range(len(s)):
-        if s[i].lower() in "aeiou":
-            res.append(str(i + 1))
-        else:
-            res.append(s[i])
+        res.append(str(i + 1) if s[i].lower() in "aeiou" else s[i])
 
     return ''.join(res)
