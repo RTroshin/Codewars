d97f58986f1abb43fb9343ec90e31cb44fe32c44
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-03-02 23:25:03 +0300

    Find twins
    Update file "Find twins.py"

--- Python/7 kyu/Find twins.py
+++ Python/7 kyu/Find twins.py
@@ -1,6 +1,6 @@
 def elimination(arr):
-    for i in range(len(arr)):
-        if arr.count(arr[i]) == 2:
-            return arr[i]
+    for n in arr:
+        if arr.count(n) == 2:
+            return n
 
     return None
