e4b41ef7d4b5ec3a1f8e80d1a883dc2387f8001f
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-03-12 23:25:06 +0300

    C.Wars
    Update file "C.Wars.py"

--- Python/7 kyu/C.Wars.py
+++ Python/7 kyu/C.Wars.py
@@ -1,3 +1,3 @@
 def initials(name):
     name = name.split()
-    return ''.join(n[0].upper() + '.' for n in name)[:-1] + name[-1][1:]
+    return '.'.join(n[0].upper() for n in name) + name[-1][1:]
