50372c80c9d1aa14a7ff763773abab99c9a3393d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-03-15 23:56:26 +0300

    Find the nth Digit of a Number
    Update file "Find the nth Digit of a Number.py"

--- Python/7 kyu/Find the nth Digit of a Number.py
+++ Python/7 kyu/Find the nth Digit of a Number.py
@@ -1,7 +1,5 @@
 def find_digit(num, nth):
     if nth < 1:
         return -1
-    elif nth > len(str(num)):
-        return 0
 
-    return int(str(abs(num))[-nth])
+    return 0 if nth > len(str(num)) else int(str(abs(num))[-nth])
