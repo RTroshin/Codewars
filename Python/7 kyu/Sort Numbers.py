db0a6e73c066825bad2ea2da9929fd0c8648e48a
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-03-23 23:40:02 +0300

    Sort Numbers
    Update file "Sort Numbers.py"

--- Python/7 kyu/Sort Numbers.py
+++ Python/7 kyu/Sort Numbers.py
@@ -1,8 +1,5 @@
 def solution(nums):
     if nums:
-        for i in range(len(nums) - 1):
-            for j in range(len(nums) - 1):
-                if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        nums.sort()
 
     return nums or []
