77bda69303a60ad0d8e5a2e6f457fe03a72b477e
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-03-31 23:55:08 +0300

    Inspiring Strings
    Update file "Inspiring Strings.py"

--- Python/7 kyu/Inspiring Strings.py
+++ Python/7 kyu/Inspiring Strings.py
@@ -2,8 +2,8 @@ def longest_word(string_of_words):
     string_of_words = string_of_words.split()
     max_word = string_of_words[0]
 
-    for i in range(len(string_of_words)):
-        if len(max_word) < len(string_of_words[i]):
-            max_word = string_of_words[i]
+    for word in string_of_words:
+        if len(max_word) < len(word):
+            max_word = word
 
     return max_word
