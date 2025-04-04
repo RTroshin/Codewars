cf5fa1c599299b2d8af85d9c0d091bafa22fbd2a
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-04-04 22:45:15 +0300

    Nickname Generator
    Update file "Nickname Generator.py"

--- Python/7 kyu/Nickname Generator.py
+++ Python/7 kyu/Nickname Generator.py
@@ -1,7 +1,5 @@
 def nickname_generator(name):
     if len(name) < 4:
         return "Error: Name too short"
-    elif name[2] in "aeiou":
-        return name[:4]
     else:
-        return name[:3]
+        return name[:4] if name[2] in "aeiou" else name[:3]
