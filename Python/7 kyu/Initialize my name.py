55c863f6158c9b6a530857a2765f787d856ea983
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-04-06 17:15:08 +0300

    Initialize my name
    Update file "Initialize my name.py"

--- Python/7 kyu/Initialize my name.py
+++ Python/7 kyu/Initialize my name.py
@@ -5,9 +5,9 @@ def initialize_names(name):
     if len(name) == 1:
         return name[0]
     elif len(name) == 2:
-        return name[0] + ' ' + name[1]
+        return f"{name[0]} {name[1]}"
 
     for i in range(1, len(name) - 1):
         res += name[i][0] + '. '
 
-    return name[0] + ' ' + res + name[-1]
+    return f"{name[0]} {res}{name[-1]}"
