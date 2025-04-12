0d0bfcba0f10620c1c19bbc58fc860afe479a022
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-04-12 18:30:23 +0300

    No ifs no buts
    Update file "No ifs no buts.py"

--- Python/7 kyu/No ifs no buts.py
+++ Python/7 kyu/No ifs no buts.py
@@ -1,4 +1,4 @@
 def no_ifs_no_buts(a, b):
     lst = [a, b]
 
-    return [[],[(str(a) + " is greater than " + str(b)), (str(a) + " is smaller than " + str(b))][lst.index(max(lst))], [(str(a) + " is equal to " + str(b))][0]][lst.count(a)]
+    return [[],[f"{a} is greater than {b}", (f"{a} is smaller than {b}")][lst.index(max(lst))], [(f"{a} is equal to {b}")][0]][lst.count(a)]
