58f953a566bd506ced44d2eb6c63b5f5e0e539af
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-04-13 21:55:03 +0300

    Initialize my name
    Update file "Initialize my name.py"

--- Python/7 kyu/Initialize my name.py
+++ Python/7 kyu/Initialize my name.py
@@ -1,7 +1,4 @@
 def initialize_names(name):
     name = name.split()
 
-    if len(name) < 3:
-        return ' '.join(name)
-
-    return f"{name[0]} {'. '.join([name[i][0] for i in range(1, len(name) - 1)])}. {name[-1]}"
+    return ' '.join(name) if len(name) < 3 else f"{name[0]} {'. '.join([name[i][0] for i in range(1, len(name) - 1)])}. {name[-1]}"
