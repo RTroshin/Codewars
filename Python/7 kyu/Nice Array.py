6e88668727df6eb10ef1f6afded5c443047e4742
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-04-17 20:20:45 +0300

    Nice Array
    Update file "Nice Array.py"

--- Python/7 kyu/Nice Array.py
+++ Python/7 kyu/Nice Array.py
@@ -2,9 +2,6 @@ def is_nice(arr):
     res = False
 
     for n in arr:
-        if n + 1 in arr or n - 1 in arr:
-            res = True
-        else:
-            res = False
+        res = True if n + 1 in arr or n - 1 in arr else False
 
     return res
