18dc5771e1f0514c66c415c5d44391701827d48c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-04-27 11:00:07 +0300

    Find the calculation type
    Update file "Find the calculation type.py"

--- Python/7 kyu/Find the calculation type.py
+++ Python/7 kyu/Find the calculation type.py
@@ -1,9 +1,2 @@
 def calc_type(a, b, res):
-    if a + b == res:
-        return "addition"
-    elif a - b == res:
-        return "subtraction"
-    elif a * b == res:
-        return "multiplication"
-    elif a / b == res:
-        return "division"
+    return {a + b: "addition", a - b: "subtraction", a * b: "multiplication", a / b: "division"}[res]
