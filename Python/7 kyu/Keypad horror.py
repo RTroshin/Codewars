c18507583e3b0a7399ebc64d74908e2898d1bc05
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-05-20 22:20:11 +0300

    Keypad horror
    Add new file "Keypad horror.py"

---
+++ Python/7 kyu/Keypad horror.py
@@ -0,0 +1,21 @@
+def computer_to_phone(numbers):
+    numbers = list(numbers)
+    res = ''
+
+    for n in numbers:
+        if n == '1':
+            res += '7'
+        elif n == '2':
+            res += '8'
+        elif n == '3':
+            res += '9'
+        elif n == '7':
+            res += '1'
+        elif n == '8':
+            res += '2'
+        elif n == '9':
+            res += '3'
+        else:
+            res += n
+
+    return res
