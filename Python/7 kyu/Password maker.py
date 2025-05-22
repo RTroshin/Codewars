9be7c5fd4d699d868bd7fb04e3e4803998900623
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-05-22 22:25:50 +0300

    Password maker
    Update file "Password maker.py"

--- Python/7 kyu/Password maker.py
+++ Python/7 kyu/Password maker.py
@@ -1,10 +1,7 @@
 def make_password(phrase):
 
-    phrase = phrase.replace('i', '1')
-    phrase = phrase.replace('I', '1')
-    phrase = phrase.replace('o', '0')
-    phrase = phrase.replace('O', '0')
-    phrase = phrase.replace('s', '5')
-    phrase = phrase.replace('S', '5')
+    phrase = phrase.replace('i', '1').replace('I', '1')
+    phrase = phrase.replace('o', '0').replace('O', '0')
+    phrase = phrase.replace('s', '5').replace('S', '5')
 
     return ''.join(word[0] for word in phrase.split())
