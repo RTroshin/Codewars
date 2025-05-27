5055e5a3f43c94ce7d65e6328496225fcc476c62
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-05-27 22:40:05 +0300

    Product Of Maximums Of Array (Array Series #2)
    Update file "Product Of Maximums Of Array (Array Series #2).py"

--- Python/7 kyu/Product Of Maximums Of Array (Array Series #2).py
+++ Python/7 kyu/Product Of Maximums Of Array (Array Series #2).py
@@ -1,8 +1,8 @@
 def max_product(lst, n_largest_elements):
-    max_prod = max(lst)
+    max_prod = 1
     max_lst = sorted(lst, reverse=True)
 
-    for i in range(1, n_largest_elements):
+    for i in range(n_largest_elements):
         max_prod *= max_lst[i]
 
     return max_prod
