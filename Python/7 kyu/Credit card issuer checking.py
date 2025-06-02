6a687dee5f5d50f2c40484a8569918347f841512
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-06-02 21:55:15 +0300

    Credit card issuer checking
    Update file "Credit card issuer checking.py"

--- Python/7 kyu/Credit card issuer checking.py
+++ Python/7 kyu/Credit card issuer checking.py
@@ -2,9 +2,9 @@ def get_issuer(num):
     if len(str(num)) == 16:
         if str(num)[0] == '4':
             return "VISA"
-        if str(num)[:4] == "6011":
+        elif str(num)[:4] == "6011":
             return "Discover"
-        if str(num)[:2] == "51" or str(num)[:2] == "52" or str(num)[:2] == "53" or str(num)[:2] == "54" or str(num)[:2] == "55":
+        elif str(num)[:2] == "51" or str(num)[:2] == "52" or str(num)[:2] == "53" or str(num)[:2] == "54" or str(num)[:2] == "55":
             return "Mastercard"
     if len(str(num)) == 13:
         if str(num)[0] == '4':
