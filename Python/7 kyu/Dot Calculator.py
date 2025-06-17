7bf7cdac9f978659e74e682947124fb855eea7d3
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-06-17 21:45:02 +0300

    Dot Calculator
    Update file "Dot Calculator.py"

--- Python/7 kyu/Dot Calculator.py
+++ Python/7 kyu/Dot Calculator.py
@@ -11,3 +11,7 @@ def calculator(txt):
         txt = txt.replace(' ', '')
         txt = txt.split('*')
         return txt[0] * len(txt[1])
+    if "//" in txt:
+        txt = txt.replace(' ', '')
+        txt = txt.split("//")
+        return txt[0][:len(txt[0]) // len(txt[1])]
