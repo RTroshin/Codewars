8534a84bba45c6848a803f788426c7c3c95034ac
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-06-19 22:00:10 +0300

    Dot Calculator
    Update file "Dot Calculator.py"

--- Python/7 kyu/Dot Calculator.py
+++ Python/7 kyu/Dot Calculator.py
@@ -1,17 +1,15 @@
 def calculator(txt):
+    txt = txt.replace(' ', '')
+
     if '+' in txt:
-        txt = txt.replace(' ', '')
         txt = txt.split('+')
         return txt[0] + txt[1]
     elif '-' in txt:
-        txt = txt.replace(' ', '')
         txt = txt.split('-')
         return txt[0][len(txt[1]):]
     elif '*' in txt:
-        txt = txt.replace(' ', '')
         txt = txt.split('*')
         return txt[0] * len(txt[1])
     elif "//" in txt:
-        txt = txt.replace(' ', '')
         txt = txt.split("//")
         return txt[0][:len(txt[0]) // len(txt[1])]
