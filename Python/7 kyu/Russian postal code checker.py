5aaa7ba7224afbd5bf68c155057a663619d99b08
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-06-29 11:45:26 +0300

    Russian postal code checker
    Update file "Russian postal code checker.py"

--- Python/7 kyu/Russian postal code checker.py
+++ Python/7 kyu/Russian postal code checker.py
@@ -1,5 +1,5 @@
 def zipvalidate(postcode):
-    if postcode[0] in "05789" and len(postcode) != 6:
+    if postcode[0] in "05789" or len(postcode) != 6:
         return False
     elif not postcode.isdecimal():
         return False
