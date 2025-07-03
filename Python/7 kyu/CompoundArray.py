4f6d72fc9bc7cffb80e654961a8bda245fbf8235
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-07-03 22:00:14 +0300

    CompoundArray
    Update file "CompoundArray.py"

--- Python/7 kyu/CompoundArray.py
+++ Python/7 kyu/CompoundArray.py
@@ -5,6 +5,4 @@ def compound_array(a, b):
         res.append(a[i])
         res.append(b[i])
 
-    res += a[: len(b)] if len(a) >= len(b) else b[: len(a)]
-
-    return res
+    return res + a[: len(b)] if len(a) >= len(b) else b[: len(a)]
