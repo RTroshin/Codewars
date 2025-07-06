10da16aa73801bf485fc97cf5d2e3580135312b0
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-07-06 18:35:15 +0300

    Baby shark lyrics generator
    Update file "Baby shark lyrics generator.py"

--- Python/7 kyu/Baby shark lyrics generator.py
+++ Python/7 kyu/Baby shark lyrics generator.py
@@ -1,26 +1,26 @@
 def baby_shark_lyrics():
-    return "Baby shark, doo doo doo doo doo doo\
-            Baby shark, doo doo doo doo doo doo\
-            Baby shark, doo doo doo doo doo doo\
-            Baby shark!\
-            Mommy shark, doo doo doo doo doo doo\
-            Mommy shark, doo doo doo doo doo doo\
-            Mommy shark, doo doo doo doo doo doo\
-            Mommy shark!\
-            Daddy shark, doo doo doo doo doo doo\
-            Daddy shark, doo doo doo doo doo doo\
-            Daddy shark, doo doo doo doo doo doo\
-            Daddy shark!\
-            Grandma shark, doo doo doo doo doo doo\
-            Grandma shark, doo doo doo doo doo doo\
-            Grandma shark, doo doo doo doo doo doo\
-            Grandma shark!\
-            Grandpa shark, doo doo doo doo doo doo\
-            Grandpa shark, doo doo doo doo doo doo\
-            Grandpa shark, doo doo doo doo doo doo\
-            Grandpa shark!\
-            Let's go hunt, doo doo doo doo doo doo\
-            Let's go hunt, doo doo doo doo doo doo\
-            Let's go hunt, doo doo doo doo doo doo\
-            Let's go hunt!\
+    return "Baby shark, doo doo doo doo doo doo\n\
+            Baby shark, doo doo doo doo doo doo\n\
+            Baby shark, doo doo doo doo doo doo\n\
+            Baby shark!\n\
+            Mommy shark, doo doo doo doo doo doo\n\
+            Mommy shark, doo doo doo doo doo doo\n\
+            Mommy shark, doo doo doo doo doo doo\n\
+            Mommy shark!\n\
+            Daddy shark, doo doo doo doo doo doo\n\
+            Daddy shark, doo doo doo doo doo doo\n\
+            Daddy shark, doo doo doo doo doo doo\n\
+            Daddy shark!\n\
+            Grandma shark, doo doo doo doo doo doo\n\
+            Grandma shark, doo doo doo doo doo doo\n\
+            Grandma shark, doo doo doo doo doo doo\n\
+            Grandma shark!\n\
+            Grandpa shark, doo doo doo doo doo doo\n\
+            Grandpa shark, doo doo doo doo doo doo\n\
+            Grandpa shark, doo doo doo doo doo doo\n\
+            Grandpa shark!\n\
+            Let's go hunt, doo doo doo doo doo doo\n\
+            Let's go hunt, doo doo doo doo doo doo\n\
+            Let's go hunt, doo doo doo doo doo doo\n\
+            Let's go hunt!\n\
             Run away,…"
