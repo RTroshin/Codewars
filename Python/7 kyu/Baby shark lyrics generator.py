02f5490d3b89687bf7e61481eb33c30c0ed8ab39
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-07-12 23:25:20 +0300

    Baby shark lyrics generator
    Update file "Baby shark lyrics generator.py"

--- Python/7 kyu/Baby shark lyrics generator.py
+++ Python/7 kyu/Baby shark lyrics generator.py
@@ -1,26 +1,14 @@
 def baby_shark_lyrics():
-    return "Baby shark, doo doo doo doo doo doo\n\
-            Baby shark, doo doo doo doo doo doo\n\
-            Baby shark, doo doo doo doo doo doo\n\
-            Baby shark!\n\
-            Mommy shark, doo doo doo doo doo doo\n\
-            Mommy shark, doo doo doo doo doo doo\n\
-            Mommy shark, doo doo doo doo doo doo\n\
-            Mommy shark!\n\
-            Daddy shark, doo doo doo doo doo doo\n\
-            Daddy shark, doo doo doo doo doo doo\n\
-            Daddy shark, doo doo doo doo doo doo\n\
-            Daddy shark!\n\
-            Grandma shark, doo doo doo doo doo doo\n\
-            Grandma shark, doo doo doo doo doo doo\n\
-            Grandma shark, doo doo doo doo doo doo\n\
-            Grandma shark!\n\
-            Grandpa shark, doo doo doo doo doo doo\n\
-            Grandpa shark, doo doo doo doo doo doo\n\
-            Grandpa shark, doo doo doo doo doo doo\n\
-            Grandpa shark!\n\
-            Let's go hunt, doo doo doo doo doo doo\n\
-            Let's go hunt, doo doo doo doo doo doo\n\
-            Let's go hunt, doo doo doo doo doo doo\n\
-            Let's go hunt!\n\
+    return "Baby shark, doo doo doo doo doo doo\n" * 3\
+            + "Baby shark!\n" +\
+            "Mommy shark, doo doo doo doo doo doo\n" * 3\
+            + "Mommy shark!\n" +\
+            "Daddy shark, doo doo doo doo doo doo\n" * 3\
+            + "Daddy shark!\n" + \
+            "Grandma shark, doo doo doo doo doo doo\n" * 3\
+            + "Grandma shark!\n" + \
+            "Grandpa shark, doo doo doo doo doo doo\n" * 3\
+            + "Grandpa shark!\n" + \
+            "Let's go hunt, doo doo doo doo doo doo\n" * 3\
+            + "Let's go hunt!\n\
             Run away,…"
