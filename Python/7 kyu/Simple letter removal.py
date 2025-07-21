33f8dff4c2fbcaba357cdb76c008f67bf2638cb5
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-07-21 20:55:42 +0300

    Simple letter removal
    Update file "Simple letter removal.py"

--- Python/7 kyu/Simple letter removal.py
+++ Python/7 kyu/Simple letter removal.py
@@ -2,18 +2,17 @@ def solve(st, k):
     st = list(st)
     alph = 'a'
     count = 0
-    i = 0
 
     while count != k:
-        if alph in st:
-            if st[i] == alph:
-                st[i] = ''
-                count += 1
-            i += 1
-        elif ord(alph) < 122:
-            alph = chr(ord(alph) + 1)
-            i = 0
-        else:
-            return ''
+        for i in range(len(st)):
+            if alph in st:
+                if st[i] == alph:
+                    st[i] = ''
+                    count += 1
+                    break
+            elif ord(alph) < 122:
+                alph = chr(ord(alph) + 1)
+            else:
+                return ''
 
     return ''.join(st)
