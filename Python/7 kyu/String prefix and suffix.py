42819297507d38242bd1c8705661f8b270f6974c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-07-24 21:45:20 +0300

    String prefix and suffix
    Update file "String prefix and suffix.py"

--- Python/7 kyu/String prefix and suffix.py
+++ Python/7 kyu/String prefix and suffix.py
@@ -1,8 +1,8 @@
 def solve(st):
-    res = -1
+    res = 0
 
-    for i in range(len(st)):
-        if st[:i] == st[::-1][:i]:
+    for i in range(len(st) - 1):
+        if st[:i + 1] == st[::-1][:i + 1] or st[:i + 1] == st[-i - 1:]:
             res += 1
 
     return res
