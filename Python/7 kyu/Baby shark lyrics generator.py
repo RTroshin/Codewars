151aa2d5116703ca793dbf99eea2cf6f574eea1d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-07-25 22:55:45 +0300

    Baby shark lyrics generator
    Update file "Baby shark lyrics generator.py"

--- Python/7 kyu/Baby shark lyrics generator.py
+++ Python/7 kyu/Baby shark lyrics generator.py
@@ -1,14 +1,8 @@
 def baby_shark_lyrics():
-    return f"Baby shark, {'doo ' * 5}doo\n" * 3\
-            + "Baby shark!\n" +\
-            f"Mommy shark, {'doo ' * 5}doo\n" * 3\
-            + "Mommy shark!\n" +\
-            f"Daddy shark, {'doo ' * 5}doo\n" * 3\
-            + "Daddy shark!\n" +\
-            f"Grandma shark, {'doo ' * 5}doo\n" * 3\
-            + "Grandma shark!\n" +\
-            f"Grandpa shark, {'doo ' * 5}doo\n" * 3\
-            + "Grandpa shark!\n" +\
-            f"Let's go hunt, {'doo ' * 5}doo\n" * 3\
-            + "Let's go hunt!\n" +\
+    return f"Baby shark, {'doo ' * 5}doo\n" * 3 + "Baby shark!\n" +\
+            f"Mommy shark, {'doo ' * 5}doo\n" * 3 + "Mommy shark!\n" +\
+            f"Daddy shark, {'doo ' * 5}doo\n" * 3 + "Daddy shark!\n" +\
+            f"Grandma shark, {'doo ' * 5}doo\n" * 3 + "Grandma shark!\n" +\
+            f"Grandpa shark, {'doo ' * 5}doo\n" * 3 + "Grandpa shark!\n" +\
+            f"Let's go hunt, {'doo ' * 5}doo\n" * 3 + "Let's go hunt!\n" +\
             "Run away,…"
