76cbae0a8118a9cf21e8cf6059596fff2d1657d2
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-08-07 08:40:28 +0300

    Baby shark lyrics generator
    Update file "Baby shark lyrics generator.py"

--- Python/7 kyu/Baby shark lyrics generator.py
+++ Python/7 kyu/Baby shark lyrics generator.py
@@ -1,9 +1,5 @@
 def baby_shark_lyrics():
     s = " shark"
     sharks = [f"Baby{s}", f"Mommy{s}", f"Daddy{s}", f"Grandma{s}", f"Grandpa{s}", "Let's go hunt"]
-    res = ''
 
-    for i in range(6):
-        res += f"{sharks[i]},{' doo' * 6}\n" * 3 + f"{sharks[i]}!\n"
-
-    return f"{res}Run away,…"
+    return ''.join([f"{sharks[i]},{' doo' * 6}\n" * 3 + f"{sharks[i]}!\n" for i in range(6)]) + "Run away,…"
