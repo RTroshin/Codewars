18fff69546ae190fa95021210e57be3066ed3ddc
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-08-17 22:00:03 +0300

    Plus - minus - plus - plus - ... - Count
    Update file "Plus - minus - plus - plus - ... - Count.py"

--- Python/7 kyu/Plus - minus - plus - plus - ... - Count.py
+++ Python/7 kyu/Plus - minus - plus - plus - ... - Count.py
@@ -3,9 +3,9 @@ def catch_sign_change(lst):
 
     for i in range(len(lst) - 1):
         if lst[i] < 0 and lst[i + 1] < 0:
-            count += 0
+            pass
         elif lst[i] >= 0 and lst[i + 1] >= 0:
-            count += 0
+            pass
         else:
             count += 1
 
