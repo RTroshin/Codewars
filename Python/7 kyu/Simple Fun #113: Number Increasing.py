72044af4d84b334165daca2534a2683e33738911
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-08-20 23:05:04 +0300

    Simple Fun #113: Number Increasing
    Add new file "Simple Fun #113: Number Increasing.py"

---
+++ Python/7 kyu/Simple Fun #113: Number Increasing.py
@@ -0,0 +1,25 @@
+def number_increasing(n: int) -> bool:
+    i = 1
+
+    if n == i:
+        return True
+    elif n == 3:
+        return True
+    elif n == 5 + 1:
+        return True
+
+    while i <= n:
+        i *= 3
+
+    if i == n:
+        return True
+
+    i = 1
+
+    while i <= n:
+        i += 5
+
+    if i == n:
+        return True
+
+    return False
