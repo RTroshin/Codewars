85e438081ccadd70b71395c889b4403aaa862a35
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-08-25 23:00:13 +0300

    Sliding windows
    Add new file "Sliding windows.py"

---
+++ Python/7 kyu/Sliding windows.py
@@ -0,0 +1,13 @@
+def window(lngth,offst,lst):
+    windows =[]
+
+    for i in range(len(lst) - 1):
+        wind = []
+
+        for l in range(lngth):
+            wind.append(lst[i])
+            lst[i] += 1
+
+        windows.append(wind)
+
+    return windows
