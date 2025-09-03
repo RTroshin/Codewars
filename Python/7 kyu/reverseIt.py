d6c85b24f8f608ab3d6e47c8638383d78f4e8cd7
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-09-03 20:25:22 +0300

    reverseIt
    Update file "reverseIt.py"

--- Python/7 kyu/reverseIt.py
+++ Python/7 kyu/reverseIt.py
@@ -1,7 +1,5 @@
 def reverse_it(data):
-    if type(data) == str:
+    if type(data) == str or type(data) == list:
         return data[::-1]
     elif type(data) == int:
         return int(str(data)[::-1])
-    elif type(data) == list:
-        return data[::-1]
