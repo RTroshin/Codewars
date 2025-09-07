d19f3ed0709d0121e5161e8aad735f00edf44030
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-09-07 22:20:33 +0300

    Every possible sum of two digits
    Update file "Every possible sum of two digits.py"

--- Python/7 kyu/Every possible sum of two digits.py
+++ Python/7 kyu/Every possible sum of two digits.py
@@ -2,7 +2,8 @@ def digits(num):
     res = []
     num = str(num)
 
-    for i in range(len(num) - 1):
-        res.append(int(num[i]) + int(num[i + 1]))
+    for i in range(len(num)):
+        for j in range(len(num)):
+            res.append(int(num[i]) + int(num[j]))
 
     return res
