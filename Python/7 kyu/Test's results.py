ce2cbc66e8cb089ca6f745774aed5a008ac72303
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-09-15 20:45:06 +0300

    Test's results
    Add new file "Test's results.py"

---
+++ Python/7 kyu/Test's results.py
@@ -0,0 +1,18 @@
+def test(r):
+    res = []
+    dictionary = {}
+    class_average = 0
+    
+    for i in range(len(r)):
+        class_average += r[i]
+
+    class_average /= len(r)
+
+    dictionary['h'] = r.count(9) + r.count(10)
+    dictionary['a'] = r.count(7) + r.count(8)
+    dictionary['l'] = r.count(1) + r.count(2) + r.count(3) + r.count(4) + r.count(5) + r.count(6)
+
+    res.append(round(class_average, 3))
+    res.append(dictionary)
+
+    return res
