a6fc84262c1ae5a767b2ff71f1e3794ad53b0d1a
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-09-19 22:00:03 +0300

    Test's results
    Update file "Test's results.py"

--- Python/7 kyu/Test's results.py
+++ Python/7 kyu/Test's results.py
@@ -1,13 +1,12 @@
 def test(r):
     res = []
     dictionary = {}
-    class_average = sum(r) / len(r)
 
     dictionary['h'] = r.count(9) + r.count(10)
     dictionary['a'] = r.count(7) + r.count(8)
     dictionary['l'] = r.count(1) + r.count(2) + r.count(3) + r.count(4) + r.count(5) + r.count(6)
 
-    res.append(round(class_average, 3))
+    res.append(round(sum(r) / len(r), 3))
     res.append(dictionary)
 
     return res
