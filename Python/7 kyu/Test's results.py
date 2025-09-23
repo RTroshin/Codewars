1c32a3efb03fb54160b9204d0ea00cd36a1cd264
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-09-23 22:55:47 +0300

    Test's results
    Update file "Test's results.py"

--- Python/7 kyu/Test's results.py
+++ Python/7 kyu/Test's results.py
@@ -3,6 +3,6 @@ def test(r):
 
     dictionary['h'] = r.count(9) + r.count(10)
     dictionary['a'] = r.count(7) + r.count(8)
-    dictionary['l'] = sum([r.count(i) for i in range(7)])
+    dictionary['l'] = sum(r.count(i) for i in range(7))
 
     return [round(sum(r) / len(r), 3), dictionary]
