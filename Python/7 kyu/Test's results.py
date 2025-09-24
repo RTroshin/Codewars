88fa3b9f80d00694772e2519fa206da1ce258376
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-09-24 20:15:16 +0300

    Test's results
    Update file "Test's results.py"

--- Python/7 kyu/Test's results.py
+++ Python/7 kyu/Test's results.py
@@ -4,5 +4,8 @@ def test(r):
     dictionary['h'] = r.count(9) + r.count(10)
     dictionary['a'] = r.count(7) + r.count(8)
     dictionary['l'] = sum(r.count(i) for i in range(7))
+    
+    if dictionary['a'] == 0 and dictionary['l'] == 0:
+        return [round(sum(r) / len(r), 3), dictionary, "They did well"]
 
     return [round(sum(r) / len(r), 3), dictionary]
