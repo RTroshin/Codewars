5f803ca3ba3ad7e72a60fe3132a18b6d798f1c73
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-09-25 19:20:11 +0300

    Capitals first!
    Add new file "Capitals first!.py"

---
+++ Python/7 kyu/Capitals first!.py
@@ -0,0 +1,15 @@
+def capitals_first(text):
+    res1 = []
+    res2 = []
+    text = text.split()
+
+    for i in range(len(text)):
+        if text[i][0].isupper():
+            res1.append(text[i])
+        if text[i][0].islower():
+            res2.append(text[i])
+
+    res = res1 + res2
+    res = ' '.join(res)
+
+    return res
