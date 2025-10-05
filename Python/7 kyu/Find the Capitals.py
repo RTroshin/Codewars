c3bcf4dc71854a13cfe620c1064948a0c578b141
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-10-05 23:15:23 +0300

    Find the Capitals
    Update file "Find the Capitals.py"

--- Python/7 kyu/Find the Capitals.py
+++ Python/7 kyu/Find the Capitals.py
@@ -2,6 +2,6 @@ def capital(capitals):
     res = []
 
     for i in range(len(capitals)):
-        res.append("The capital of " + capitals[i].get("state") + " is " + capitals[i].get("capital"))
+        res.append("The capital of " + (capitals[i].get("state") or capitals[i].get("country")) + " is " + capitals[i].get("capital"))
 
     return res
