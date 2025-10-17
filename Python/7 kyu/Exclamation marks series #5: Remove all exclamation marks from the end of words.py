b76fabe87cf03056d047e6090f424499673a9165
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-10-17 23:20:03 +0300

    Exclamation marks series #5: Remove all exclamation marks from the end of words
    Update file "Exclamation marks series #5: Remove all exclamation marks from the end of words.py"

--- Python/7 kyu/Exclamation marks series #5: Remove all exclamation marks from the end of words.py
+++ Python/7 kyu/Exclamation marks series #5: Remove all exclamation marks from the end of words.py
@@ -1,10 +1,2 @@
-def remove(str):
-    res = ''
-
-    for ch in str[::-1]:
-        if ch == '!':
-            res += ''
-        else:
-            res += ch
-
-    return res[::-1]
+def remove(st):
+    return st.rstrip('!')
