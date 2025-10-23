2c231ad0189f22d953fbf8b6701cad4c0f335ace
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-10-23 23:35:09 +0300

    Disarium Number (Special Numbers Series #3)
    Update file "Disarium Number (Special Numbers Series #3).py"

--- Python/7 kyu/Disarium Number (Special Numbers Series #3).py
+++ Python/7 kyu/Disarium Number (Special Numbers Series #3).py
@@ -1,8 +1,8 @@
-def disarium_number(number):
-    number = str(number)
+def disarium_number(num):
+    num = str(num)
     sum = 0
 
-    for i in range(len(number)):
-        sum += int(number[i]) ** (i + 1)
+    for i in range(len(num)):
+        sum += int(num[i]) ** (i + 1)
 
-    return "Disarium !!" if str(sum) == number else "Not !!"
+    return "Disarium !!" if str(sum) == num else "Not !!"
