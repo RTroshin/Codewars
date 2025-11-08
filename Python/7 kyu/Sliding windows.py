faa06fa89ab5d585d511ddf24ea9b69968364de8
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-11-08 23:40:45 +0300

    Sliding windows
    Update file "Sliding windows.py"

--- Python/7 kyu/Sliding windows.py
+++ Python/7 kyu/Sliding windows.py
@@ -1,10 +1,6 @@
 def window(lngth, offst, lst):
     windows =[lst[i: i + lngth] for i in range(len(lst) - 1)]
 
-    windowsRes = []
-
-    for i in range(0, len(lst) - 1, offst):
-        windowsRes.append(windows[i])
-        
+    windowsRes = [windows[i] for i in range(0, len(lst) - 1, offst)]
 
     return windowsRes
