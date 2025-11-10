a1dc4452bb04e9c52c5096d37a6f0c4fd3fae6d1
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-11-10 23:55:15 +0300

    Correct the time-string
    Add new file "Correct the time-string.py"

---
+++ Python/7 kyu/Correct the time-string.py
@@ -0,0 +1,10 @@
+def time_correct(t):
+    if not t:
+        return t
+
+    t = t.split(':')
+
+    if len(t) != 3:
+        return None
+    if not t[0].isdecimal() or not t[1].isdecimal() or not t[0].isdecimal():
+        return None
