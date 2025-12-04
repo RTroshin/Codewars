0b971cdfa499ec34316d780c7ab675993cb2f7d8
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-12-04 23:25:19 +0300

    Bubblesort Once
    Update file "Bubblesort Once.py"

--- Python/7 kyu/Bubblesort Once.py
+++ Python/7 kyu/Bubblesort Once.py
@@ -1,8 +1,6 @@
 def bubblesort_once(l):
     for i in range(len(l) - 1):
         if l[i] > l[i + 1]:
-            tmp = l[i]
-            l[i] = l[i + 1]
-            l[i + 1] = tmp
+            l[i], l[i + 1] = l[i + 1], l[i]
 
     return l
