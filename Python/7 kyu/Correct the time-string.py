370f970769ff39004f184dec74bf82dc3c7be97a
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-12-07 22:25:34 +0300

    Correct the time-string
    Update file "Correct the time-string.py"

--- Python/7 kyu/Correct the time-string.py
+++ Python/7 kyu/Correct the time-string.py
@@ -16,8 +16,8 @@ def time_correct(t):
     while int(t[0]) > 23:
         t[0] = str(int(t[0]) - 24)
 
-    if t[0] == '0':
-        t[0] += '0'
+    if len(t[0]) < 2:
+        t[0] = '0' + t[0]
     if t[1] == '0':
         t[1] += '0'
     if t[2] == '0':
