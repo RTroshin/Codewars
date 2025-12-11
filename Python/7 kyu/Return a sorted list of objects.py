b49fe09f9732199440867199a84112173555587d
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-12-11 23:00:12 +0300

    Return a sorted list of objects
    Update file "Return a sorted list of objects.py"

--- Python/7 kyu/Return a sorted list of objects.py
+++ Python/7 kyu/Return a sorted list of objects.py
@@ -2,8 +2,6 @@ def sort_list(sort_by, lst):
     for i in range(len(lst) - 1):
         for j in range(len(lst) - 1):
             if lst[j].get(sort_by) > lst[j + 1].get(sort_by):
-                temp = lst[j]
-                lst[j] = lst[j + 1]
-                lst[j + 1] = temp
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
 
     return lst
