9c0cf2b8e1e771c78754aa39ac63fe949cb2f55c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-12-15 23:15:05 +0300

    Difference between two collections
    Update file "Difference between two collections.py"

--- Python/7 kyu/Difference between two collections.py
+++ Python/7 kyu/Difference between two collections.py
@@ -3,5 +3,7 @@ def diff(a, b):
     a = list(set(a))
     b = list(set(b))
     ab = a + b
+    res = [n for n in ab if ab.count(n) == 1]
+    res.sort()
 
-    return [n for n in ab if ab.count(n) == 1]
+    return res
