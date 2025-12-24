d3d7403034717cc219f64c9735feff19eba1d8f3
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-12-24 17:00:19 +0300

    99 Bottles of Beer
    Update file "99 Bottles of Beer.py"

--- Python/7 kyu/99 Bottles of Beer.py
+++ Python/7 kyu/99 Bottles of Beer.py
@@ -1,12 +1,19 @@
 def sing():
-    return ["99 bottles of beer on the wall, 99 bottles of beer.",
-            "Take one down and pass it around, 98 bottles of beer on the wall.",
-            "98 bottles of beer on the wall, 98 bottles of beer.",
-            "3 bottles of beer on the wall, 3 bottles of beer.",
-            "Take one down and pass it around, 2 bottles of beer on the wall.",
-            "2 bottles of beer on the wall, 2 bottles of beer.",
-            "Take one down and pass it around, 1 bottle of beer on the wall.",
-            "1 bottle of beer on the wall, 1 bottle of beer.",
-            "Take one down and pass it around, no more bottles of beer on the wall.",
-            "No more bottles of beer on the wall, no more bottles of beer.",
-            "Go to the store and buy some more, 99 bottles of beer on the wall."]
+    res = []
+
+    for i in range(99, 1, -1):
+        if i > 0:
+            res.append(f"{i} bottles of beer on the wall, {i} bottles of beer.")
+            res.append(f"Take one down and pass it around, {i - 1} bottles of beer on the wall.")
+        else:
+            res.append("No more bottles of beer on the wall, no more bottles of beer.")
+            res.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
+
+    for i in range(1000, 1, -1):
+        if i > 99:
+            res.append("No more bottles of beer on the wall, no more bottles of beer.")
+            res.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
+        else:
+            break
+
+    return res
