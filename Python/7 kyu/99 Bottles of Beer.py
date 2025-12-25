5b00c3a493780374f9bb5ce15204b288effe745e
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-12-25 22:20:28 +0300

    99 Bottles of Beer
    Update file "99 Bottles of Beer.py"

--- Python/7 kyu/99 Bottles of Beer.py
+++ Python/7 kyu/99 Bottles of Beer.py
@@ -13,7 +13,5 @@ def sing():
         if i > 99:
             res.append("No more bottles of beer on the wall, no more bottles of beer.")
             res.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
-        else:
-            break
 
     return res
