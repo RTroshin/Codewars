417816094c414858f08a3daaae45aa18119c7235
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2025-12-28 23:45:18 +0300

    99 Bottles of Beer
    Update file "99 Bottles of Beer.py"

--- Python/7 kyu/99 Bottles of Beer.py
+++ Python/7 kyu/99 Bottles of Beer.py
@@ -2,11 +2,11 @@ def sing():
     res = []
 
     for i in range(101, 1, -1):
-        if i > 0 and i < 100:
+        if i > 0 and i < 101:
             res.append(f"{i} bottles of beer on the wall, {i} bottles of beer.")
             res.append(f"Take one down and pass it around, {i - 1} bottles of beer on the wall.")
-        else:
-            res.append("No more bottles of beer on the wall, no more bottles of beer.")
-            res.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
+
+    res.append("No more bottles of beer on the wall, no more bottles of beer.")
+    res.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
 
     return res
