c74e3362098abb72b5b5d70a5d21119ce1379912
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-01-01 23:55:28 +0300

    99 Bottles of Beer
    Update file "99 Bottles of Beer.py"

--- Python/7 kyu/99 Bottles of Beer.py
+++ Python/7 kyu/99 Bottles of Beer.py
@@ -1,16 +1,16 @@
 def sing():
     res = []
 
-    for i in range(101, 1, -1):
-        if i - 1 == 1:
-            res.append(f"{i - 1} bottle of beer on the wall, {i - 1} bottle of beer.")
+    for i in range(99):
+        if 99 - i == 1:
+            res.append(f"{99 - i} bottle of beer on the wall, {99 - i} bottle of beer.")
             res.append(f"Take one down and pass it around, no more bottles of beer on the wall.")
-        elif i - 2 == 1:
-            res.append(f"{i - 1} bottles of beer on the wall, {i - 1} bottles of beer.")
-            res.append(f"Take one down and pass it around, {i - 2} bottle of beer on the wall.")
-        elif i > 0 and i < 101:
-            res.append(f"{i - 1} bottles of beer on the wall, {i - 1} bottles of beer.")
-            res.append(f"Take one down and pass it around, {i - 2} bottles of beer on the wall.")
+        elif 98 - i == 1:
+            res.append(f"{99 - i} bottles of beer on the wall, {99 - i} bottles of beer.")
+            res.append(f"Take one down and pass it around, {98 - i} bottle of beer on the wall.")
+        else:
+            res.append(f"{99 - i} bottles of beer on the wall, {99 - i} bottles of beer.")
+            res.append(f"Take one down and pass it around, {98 - i} bottles of beer on the wall.")
 
     res.append("No more bottles of beer on the wall, no more bottles of beer.")
     res.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
