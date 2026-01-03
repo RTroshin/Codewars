08cec8cc7ca845328706783df2a34e6343dab019
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-01-03 23:50:26 +0300

    99 Bottles of Beer
    Update file "99 Bottles of Beer.py"

--- Python/7 kyu/99 Bottles of Beer.py
+++ Python/7 kyu/99 Bottles of Beer.py
@@ -1,17 +1,14 @@
 def sing():
     res = []
 
-    for i in range(99):
-        if 99 - i == 1:
-            res.append(f"{99 - i} bottle of beer on the wall, {99 - i} bottle of beer.")
-            res.append(f"Take one down and pass it around, no more bottles of beer on the wall.")
-        else:
-            res.append(f"{99 - i} bottles of beer on the wall, {99 - i} bottles of beer.")
+    for i in range(97):
+        res.append(f"{99 - i} bottles of beer on the wall, {99 - i} bottles of beer.")
+        res.append(f"Take one down and pass it around, {98 - i} bottles of beer on the wall.")
 
-            if 98 - i == 1:
-                res.append(f"Take one down and pass it around, {98 - i} bottle of beer on the wall.")
-            else:
-                res.append(f"Take one down and pass it around, {98 - i} bottles of beer on the wall.")
+    res.append(f"2 bottle of beer on the wall, 2 bottle of beer.")
+    res.append(f"Take one down and pass it around, 1 bottle of beer on the wall.")
+    res.append(f"1 bottle of beer on the wall, 1 bottle of beer.")
+    res.append(f"Take one down and pass it around, no more bottles of beer on the wall.")
 
     res.append("No more bottles of beer on the wall, no more bottles of beer.")
     res.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
