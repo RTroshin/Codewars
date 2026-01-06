b6b6f410ce3e489e546c83bc7efb2f064bc0cb42
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-01-06 23:00:06 +0300

    Dot Calculator
    Update file "Dot Calculator.py"

--- Python/7 kyu/Dot Calculator.py
+++ Python/7 kyu/Dot Calculator.py
@@ -1,14 +1,14 @@
 def calculator(txt):
     str = txt.split()
-    x = str[0]
-    y = str[2]
+    x = len(str[0])
+    y = len(str[2])
     symb = (str[1])
 
     if symb == '+':
-        return str[0] + str[2]
+        return '.' * (x + y)
     elif symb == '-':
-        return str[0][len(str[2]):]
+        return '.' * (x - y)
     elif symb == '*':
-        return str[0] * len(str[2])
+        return '.' * (x * y)
     elif symb == '//':
-        return str[0][:len(str[0]) // len(str[2])]
+        return '.' * int(x / y)
