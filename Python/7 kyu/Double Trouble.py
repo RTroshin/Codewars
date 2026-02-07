7d06612dd0a81de077b9a5d0a2c587e8b06e352c
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-02-07 23:45:24 +0300

    Double Trouble
    Update file "Double Trouble.py"

--- Python/7 kyu/Double Trouble.py
+++ Python/7 kyu/Double Trouble.py
@@ -4,7 +4,7 @@ def trouble(x, t):
     for i in range(len(x) - 1):
         for j in range(len(x) - 1):
             if x[j] + x[j + 1] == t:
-                res = x[:j] + x[j + 1:]
+                res = x[:j + 1] + x[j + 2:]
         x = res
 
     return res
