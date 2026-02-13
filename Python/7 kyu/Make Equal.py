0408a4295a03ad3d828ac0c81dcb59b9c35cda07
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-02-13 23:25:20 +0300

    Make Equal
    Update file "Make Equal.py"

--- Python/7 kyu/Make Equal.py
+++ Python/7 kyu/Make Equal.py
@@ -3,15 +3,17 @@ def count(a, t, x):
 
     for i in range(len(a)):
         nAdd = nSub = a[i]
-        while nAdd < t:
-            nAdd += x
-            if nAdd == t:
-                count += 1
-                break
-        while nSub > t:
-            nSub -= x
-            if nSub == t:
-                count += 1
-                break
+        if a[i] <= t:
+            while nAdd < t:
+                nAdd += x
+                if nAdd == t:
+                    count += 1
+                    break
+        else:
+            while nSub > t:
+                nSub -= x
+                if nSub == t:
+                    count += 1
+                    break
 
     return count
