0f4fb7adaab222e63458f136ace73e73e55fb887
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-02-17 22:30:31 +0300

    Translate anything into French !
    Update file "Translate anything into French !.py"

--- Python/7 kyu/Translate anything into French !.py
+++ Python/7 kyu/Translate anything into French !.py
@@ -1,8 +1,5 @@
 def i_speak_french(sentence):
-    res = []
     sentence = sentence.split()
     lenSentence = len(sentence) - 1
 
-    res = ["Baguette"] + ["baguette"] * lenSentence + ["Encore!"]
-
-    return ' '.join(res)
+    return ' '.join(["Baguette"] + ["baguette"] * lenSentence + ["Encore!"])
