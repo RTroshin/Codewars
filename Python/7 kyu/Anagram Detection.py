ad1cbb7f9e832f722e0023e76fc763ddbe3b752e
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-02-26 22:15:23 +0300

    Anagram Detection
    Update file "Anagram Detection.py"

--- Python/7 kyu/Anagram Detection.py
+++ Python/7 kyu/Anagram Detection.py
@@ -3,6 +3,9 @@ def is_anagram(test, original):
     if len(test) != len(original):
         return False
 
+    test == list(test)
+    original = list(original)
+
     if sorted(test) == sorted(original):
         return True
     else:
