ec2479b38cdff1bcc79bebd2aab38219bdfd3790
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-03-05 22:55:26 +0300

    Basic Calculator
    Update file "Basic Calculator.py"

--- Python/7 kyu/Basic Calculator.py
+++ Python/7 kyu/Basic Calculator.py
@@ -4,6 +4,9 @@ def calculate(num1, operation, num2):
     if operation == '-':
         return num1 - num2
     if operation == '/':
-        return num1 / num2
+        if num2 == 0:
+            return None
+        else:
+            return num1 / num2
     if operation == '*':
         return num1 * num2
