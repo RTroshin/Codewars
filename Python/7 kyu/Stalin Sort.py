98855092ba036cd4f65c0bba676128cd03f3d529
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-03-18 22:35:55 +0300

    Stalin Sort
    Update file "Stalin Sort.py"

--- Python/7 kyu/Stalin Sort.py
+++ Python/7 kyu/Stalin Sort.py
@@ -1,8 +1,7 @@
 def stalin_sort(arr):
-    res = [arr[0]]
 
     for i in range(1, len(arr) - 1):
-        if arr[i] < arr[i + 1]:
-            res.append(arr[i])
+        if arr[i] > arr[i + 1]:
+            arr[i] = arr[i + 1]
 
-    return res
+    return arr
