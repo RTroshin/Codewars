b081c1cf734367308fb9bafda00fc3a8edb2ccb1
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-03-25 22:45:03 +0300

    Mutate My Strings
    Add new file "Mutate My Strings.py"

---
+++ Python/7 kyu/Mutate My Strings.py
@@ -0,0 +1,14 @@
+def mutate_my_strings(s1,s2):
+    res = [s1]
+
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            s1 = list(s1)
+            s1[i] = s2[i]
+            s1 = ''.join(s1)
+            res.append(s1)
+
+    res = '\n'.join(res)
+    res += '\n'
+
+    return res
