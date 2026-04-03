8a3698c62c6b8bc5c5bd30870f04d3d6ddf279c3
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-04-03 23:45:07 +0300

    All Inclusive?
    Update file "All Inclusive?.py"

--- Python/7 kyu/All Inclusive?.py
+++ Python/7 kyu/All Inclusive?.py
@@ -1,5 +1,13 @@
 def contain_all_rots(strng, arr):
+    res = 0
+
     if not strng:
         return False
     if len(strng) < len(arr):
         return False
+
+    for i in range(len(strng)):
+        if strng[:i] + strng[i + 1:] + strng[i] in arr:
+            res += 1
+
+    return res == len(strng)
