926a7f275442bb341aacf4aa3b71ffe8b0ebd02e
Roman Troshin <R.R.Troshin@outlook.com>
2026-04-13 23:05:14 +0300

    Count the Digit
    Update file "Count the Digit.py"

--- Python/7 kyu/Count the Digit.py
+++ Python/7 kyu/Count the Digit.py
@@ -1,7 +1,7 @@
 Count the Digitdef nb_dig(n, d):
-    res_count = 0
+    res = 0
 
     for n in [i * i for i in range(n + 1)]:
-        res_count += str(n.count(str(d))
+        res += str(n.count(str(d))
 
-    return res_count
+    return res
