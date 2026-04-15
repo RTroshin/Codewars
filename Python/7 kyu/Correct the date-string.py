59f060339d8478058fd94d9036ba24323c541fd3
Roman Troshin <59419675+RTroshin@users.noreply.github.com>
2026-04-15 23:30:05 +0300

    Correct the date-string
    Add new file "Correct the date-string.py"

---
+++ Python/7 kyu/Correct the date-string.py
@@ -0,0 +1,10 @@
+def date_correct(date):
+    if not date:
+        return date
+
+    date = date.split('.')
+
+    if len(date) != 3:
+        return None
+    if not date[0].isdecimal() or not date[1].isdecimal() or not date[0].isdecimal():
+        return None
