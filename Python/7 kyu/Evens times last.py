ac265f5be471a7fed8914fe037d88d8d88a8c136
Roman Troshin <R.R.Troshin@outlook.com>
2026-04-20 22:00:23 +0300

    Evens times last
    Add new file "Evens times last.py"

---
+++ Python/7 kyu/Evens times last.py
@@ -0,0 +1,11 @@
+def even_last(numbers):
+    res = []
+
+    for i in range(len(numbers)):
+        if not i % 2:
+            res.append(numbers[i])
+
+    res = sum(res)
+    res *= numbers[len(numbers) - 1]
+
+    return res
