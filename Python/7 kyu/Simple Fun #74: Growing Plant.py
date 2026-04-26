4bf12f8109ad3e3ca4da263a1b1f6d322f8d9d70
Roman Troshin <R.R.Troshin@outlook.com>
2026-04-26 15:05:09 +0300

    Simple Fun #74: Growing Plant
    Update file "Simple Fun #74: Growing Plant.py"

--- Python/7 kyu/Simple Fun #74: Growing Plant.py
+++ Python/7 kyu/Simple Fun #74: Growing Plant.py
@@ -5,6 +5,10 @@ def growing_plant(up_speed, down_speed, desired_height):
     while height < desired_height:
         days += 1
         height += up_speed
+        
+        if height == desired_height:
+            break
+        
         height -= down_speed
     
     return days
