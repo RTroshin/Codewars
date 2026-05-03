88c18d5b6d2f6f8a425ecd4f7acd7c8bc2553e4e
Roman Troshin <R.R.Troshin@outlook.com>
2026-05-03 19:00:49 +0300

    How Many Unique Consonants?
    Update file "How Many Unique Consonants?.py"

--- Python/7 kyu/How Many Unique Consonants?.py
+++ Python/7 kyu/How Many Unique Consonants?.py
@@ -2,8 +2,8 @@ def count_consonants(text):
     count = 0
     text = text.lower()
     
-    for i in range(len(text)):
-        if not text[i] in "aeiou":
+    for ch in text:
+        if not ch in "aeiou":
             count += 1
 
     return count
